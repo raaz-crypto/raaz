908067ffbdb0a16bba2bed4009949c89a36fe922
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-09-18 11:32:32 +0530

    added a function to free hash contexts.

--- raaz-primitives/Raaz/Hash.hs
+++ raaz-primitives/Raaz/Hash.hs
@@ -10,7 +10,9 @@ module Raaz.Hash
        ( CryptoHash(..)
        ) where
 
+import Control.Exception(finally)
 import qualified Data.ByteString as B
+import System.IO.Unsafe(unsafePerformIO)
 
 
 import Raaz.Types
@@ -39,6 +41,10 @@ class ( Eq          (Hash h)
   -- | Alloc a new context for use.
   newHashCxt     :: h -> IO (HashCxt h)
 
+  -- | Free the resource associated with a context. Use of the context
+  -- again leads to undefined behaviour.
+  freeHashCxt    :: h -> HashCxt h -> IO ()
+
   -- | Resets the context for reuse in the next hashing.
   resetHashCxt   :: h -> HashCxt h -> IO ()
 
@@ -59,4 +65,13 @@ class ( Eq          (Hash h)
                     -> Int       -- ^ Number of hash blocks (not the
                                  -- number of bytes).
                     -> IO ()
+
+
+-- | Run a computation using a hash cxt
+withHashCxt :: CryptoHash h
+            => h
+            -> (HashCxt h -> IO a)  -- ^ The action to run
+            -> IO a
+withHashCxt h act = do cxt <- newHashCxt h
+                       act cxt `finally` freeHashCxt h cxt
 
