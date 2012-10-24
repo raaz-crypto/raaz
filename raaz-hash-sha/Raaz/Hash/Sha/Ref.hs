296a6c051b468fac93932cacc30cd14f7174c39f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-10-24 17:00:16 +0530

    added skeleton refrence module

---
+++ raaz-hash-sha/Raaz/Hash/Sha/Ref.hs
@@ -0,0 +1,12 @@
+{-|
+
+This module provides the reference implementation of the SHA family of
+hash functions. Depending on your platform there might be a more
+efficient implementation. So you /should not/ be using this code in
+production.
+
+-}
+
+module Raaz.Hash.Sha.Ref
+       (
+       ) where
