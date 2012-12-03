26e70bd73e87e540ab363494c259950dfedc832f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2012-12-03 13:06:56 +0530

    now uses allHashTests instead of explicit test list

--- raaz-hash-sha/tests/Modules/Sha1.hs
+++ raaz-hash-sha/tests/Modules/Sha1.hs
@@ -11,8 +11,4 @@ import Raaz.Test.Hash
 import Raaz.Primitives.Hash.Sha
 
 
-tests = [ testStoreLoad (undefined :: SHA1)
-        , testPadLengthVsPadding (undefined :: SHA1)
-        , testLengthDivisibility (undefined :: SHA1)
-        ]
-
+tests = allHashTests (undefined ::SHA1)
