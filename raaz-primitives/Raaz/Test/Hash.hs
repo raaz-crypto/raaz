ed7a4245f5e27872b51bf099dbbfb9d151fccfbe
satvikc <mystic.satvik@gmail.com>
2013-01-02 14:30:12 +0530

    StandardHashValues test added. fixes #11

--- raaz-primitives/Raaz/Test/Hash.hs
+++ raaz-primitives/Raaz/Test/Hash.hs
@@ -7,6 +7,7 @@ Generic tests for Hash implementations.
 module Raaz.Test.Hash
        ( testPadLengthVsPadding
        , testLengthDivisibility
+       , testStandardHashValues
        , allHashTests
        ) where
 
@@ -15,12 +16,16 @@ import Data.Typeable
 import Data.Word
 import Test.Framework(Test)
 import Test.Framework.Providers.QuickCheck2(testProperty)
+import Test.HUnit ((@?=))
+import Test.Framework.Providers.HUnit(testCase)
 import Test.QuickCheck(Arbitrary)
 
-import Raaz.Primitives
 import Raaz.Hash
-import Raaz.Types
+import Raaz.Primitives
 import Raaz.Test.CryptoStore
+import Raaz.Types
+import Raaz.Util.ByteString (toHex)
+
 
 -- | All generic tests for an instance of `Hash`.
 allHashTests :: ( Arbitrary h, Show h, Hash h, Typeable h )
@@ -52,3 +57,14 @@ testLengthDivisibility :: ( Hash h, Typeable h ) => h -> Test
 testLengthDivisibility h = testProperty name
                            $ prop_LengthDivisibility h
     where name = show (typeOf h) ++ ": padding + message length vs block length"
+
+-- | For a hash implementation and given prehashed input-output pairs,
+-- this function checks if the implementation satisfies those.
+testStandardHashValues :: (CryptoStore h, Hash h, Typeable h)
+                       => h
+                       -> [(B.ByteString,B.ByteString)]
+                       -> Test
+testStandardHashValues h = testCase msg . sequence_ . map checkHash
+  where getHash a = toHex( hashByteString a `asTypeOf` h)
+        msg = show (typeOf h) ++ ": StandardHashValues"
+        checkHash (a,b) = getHash a @?= b
