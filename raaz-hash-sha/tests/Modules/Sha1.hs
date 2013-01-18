951739182c8e3fb9f733a49053068441db1f39f1
satvikc <mystic.satvik@gmail.com>
2013-01-19 00:33:24 +0530

    standard Strings added for SHA1 hash check

--- raaz-hash-sha/tests/Modules/Sha1.hs
+++ raaz-hash-sha/tests/Modules/Sha1.hs
@@ -3,6 +3,8 @@ module Modules.Sha1
        ) where
 
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 import Test.Framework
 
 
@@ -13,3 +15,23 @@ import Raaz.Hash.Instances
 
 
 tests = allHashTests (undefined ::SHA1)
+     ++ testStandardHashValues (undefined ::SHA1) exampleStrings
+
+
+exampleStrings :: [(B.ByteString,B.ByteString)]
+exampleStrings = map convertToByteString
+  [ ( "abc"
+    , "a9993e364706816aba3e25717850c26c9cd0d89d" )
+  , ( "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"
+    , "84983e441c3bd26ebaae4aa1f95129e5e54670f1" )
+  , ( "The quick brown fox jumps over the lazy dog"
+    , "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12" )
+  , ( "The quick brown fox jumps over the lazy cog"
+    , "de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3" )
+  , ( ""
+    , "da39a3ee5e6b4b0d3255bfef95601890afd80709" )
+  , ( "The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog The quick brown fox jumps over the lazy dog"
+    , "5957a404e7e74dc746bea2d0d47645ddb387a7de" )
+  ]
+ where
+   convertToByteString (a,b) = (C8.pack a, C8.pack b)
