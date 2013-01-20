e41fce0cc2376d035e38fa50326c13e99ce86afd
satvikc <mystic.satvik@gmail.com>
2013-01-20 14:33:45 +0530

    sha224 tests added

--- raaz-hash-sha/tests/Main.hs
+++ raaz-hash-sha/tests/Main.hs
@@ -2,6 +2,7 @@ import Data.Version
 
 import Paths_raaz_hash_sha(version)
 import qualified Modules.Sha1 as Sha1
+import qualified Modules.Sha224 as Sha224
 import qualified Modules.Sha256 as Sha256
 import Test.Framework (defaultMain, testGroup)
 
@@ -13,5 +14,6 @@ main = do putStrLn $ "Running tests for " ++ pkgName
           defaultMain tests
 
 tests = [ testGroup "Raaz.Hash.Sha:Sha1" Sha1.tests
+        , testGroup "Raaz.Hash.Sha:Sha224" Sha224.tests
         , testGroup "Raaz.Hash.Sha:Sha256" Sha256.tests
         ]
