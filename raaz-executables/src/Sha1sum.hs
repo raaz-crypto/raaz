466c2e451af0cea5e86b183e2d599d9d5f52428c
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-01-26 16:07:57 +0530

    cleaning up sha1sum code

--- raaz-executables/src/Sha1sum.hs
+++ raaz-executables/src/Sha1sum.hs
@@ -1,18 +1,36 @@
-{-
-Provides executable to perform sha family checksums of files.
+{-|
+
+This program compute the sha1hash of the files listed on its command
+line.  It can be uses as a drop in replacement for the sha1sum program
+that is available on most Unices.
+
 -}
+
+{-# LANGUAGE OverloadedStrings  #-}
 {-# LANGUAGE DeriveDataTypeable #-}
 
-import Control.Applicative
-import Raaz.Hash
-import Raaz.Hash.Instances ()
-import Raaz.Hash.Sha
-import Raaz.Util.ByteString
-
+import Control.Monad(forM_)
 import qualified Data.ByteString.Char8 as BC
-import System.Environment
+import System.Environment(getArgs)
+import System.IO(stdin)
+
+import Raaz.Hash(hashFile, hashFileHandle, Hash)
+import Raaz.Hash.Sha(SHA1)
+import Raaz.Util.ByteString(toHex)
+
 
 main :: IO ()
-main = do
-  (fp:_) <- getArgs
-  BC.putStrLn =<< toHex <$> (hashFile fp :: IO SHA1)
+main = do args <- getArgs
+          if null args then printHashText sha1 "-"
+             else forM_ args $ printHashText sha1
+  where sha1 = undefined :: SHA1
+
+computeHash :: Hash h => String -> IO h
+computeHash file | file == "-" = hashFileHandle stdin
+                 | otherwise   = hashFile file
+
+printHashText :: Hash h => h -> String -> IO ()
+printHashText u file = do h <- computeHash file
+                          BC.putStr $ toHex $ asTypeOf h u
+                          BC.putStr "  "
+                          putStrLn file
