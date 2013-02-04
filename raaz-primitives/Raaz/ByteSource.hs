95d69bdb8e04e2a6a649ed32b20a9d035b2a993e
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-02-04 11:22:04 +0530

    added the bytesource type class

---
+++ raaz-primitives/Raaz/ByteSource.hs
@@ -0,0 +1,69 @@
+{-|
+
+-}
+module Raaz.ByteSource
+       ( FillResult(..)
+       , ByteSource(..)
+       ) where
+
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+import           Prelude hiding(length)
+import           System.IO(Handle)
+
+import           Raaz.Types(BYTES, CryptoPtr)
+import           Raaz.Util.ByteString( unsafeCopyToCryptoPtr
+                                     , unsafeNCopyToCryptoPtr
+                                     , length
+                                     )
+import           Raaz.Util.Ptr(movePtr, hFillBuf)
+
+
+-- | This type captures the result of a fill operation.
+data FillResult a = Remaining a           -- ^ only partially filled
+                                          -- the buffer.
+                  | Exhausted (BYTES Int) -- ^ source exhausted and
+                                          -- there is still so much of
+                                          -- bytes left in the buffer
+instance Functor FillResult where
+  fmap f (Remaining a ) = Remaining $ f a
+  fmap _ (Exhausted sz) = Exhausted sz
+
+class ByteSource src where
+  fillBytes :: BYTES Int  -- ^ Buffer size
+            -> src        -- ^ The source to fill.
+            -> CryptoPtr  -- ^ Buffer pointer
+            -> IO (FillResult src)
+
+instance ByteSource Handle where
+  fillBytes sz hand cptr = do
+            count <- hFillBuf hand cptr sz
+            if count < sz then return $ Exhausted $ sz - count
+               else return $ Remaining hand
+
+instance ByteSource B.ByteString where
+  fillBytes sz bs cptr | l < sz    = do unsafeCopyToCryptoPtr bs cptr
+                                        return $ Exhausted $ sz - l
+                       | otherwise = do unsafeNCopyToCryptoPtr sz bs cptr
+                                        return $ Remaining rest
+       where l    = length bs
+             rest = B.drop (fromIntegral sz) bs
+
+instance ByteSource L.ByteString where
+  fillBytes sz bs cptr =   fillBytes sz (L.toChunks bs) cptr
+                       >>= return . fmap L.fromChunks
+
+instance ByteSource src => ByteSource (Maybe src) where
+  fillBytes sz ma cptr = maybe exhausted fillIt ma
+          where exhausted = return $ Exhausted sz
+                fillIt a  =  fillBytes sz a cptr
+                          >>= return . fmap Just
+
+
+instance ByteSource src => ByteSource [src] where
+  fillBytes sz []     _    = return $ Exhausted sz
+  fillBytes sz (x:xs) cptr = do
+            result <- fillBytes sz x cptr
+            case result of
+                 Exhausted nSz -> fillBytes nSz xs $ movePtr cptr $ sz - nSz
+                 Remaining nx  -> return $ Remaining $ nx:xs
