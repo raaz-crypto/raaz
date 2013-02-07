e016f9adc2705a34c899a7da7f42155eb6606139
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-02-07 13:42:52 +0530

    added the HMAC datatype and instances for BlockPrimitives and HasPadding

--- raaz-primitives/Raaz/MAC.hs
+++ raaz-primitives/Raaz/MAC.hs
@@ -4,12 +4,16 @@ This module provides the message authentication abstraction
 
 -}
 
-{-# LANGUAGE TypeFamilies     #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Raaz.MAC
        ( MAC(..)
+       , HMAC(..)
        ) where
+import Control.Applicative
 import Data.ByteString(ByteString)
+import Foreign.Storable
 
 import Raaz.Types
 import Raaz.Primitives
@@ -34,10 +38,61 @@ class ( BlockPrimitive m
   data MACSecret m :: *
 
   -- | Convert a bytestring to a secret
-  toMACSecret  :: ByteString -> MACSecret m
+  toMACSecret  :: m -> ByteString -> MACSecret m
 
   -- | The starting MAC context
-  startMAC     :: MACSecret m -> Cxt m
+  startMACCxt  :: MACSecret m -> Cxt m
 
   -- | Finalise the context to a MAC value.
   finaliseMAC  :: MACSecret m -> Cxt m -> m
+
+
+-- | The HMAC associated to a hash value. The `Eq` instance for HMAC
+-- is essentially the `Eq` instance for the underlying hash and hence
+-- is safe against timing attack (provided the underlying hashs
+-- comparison is safe under timing attack).
+newtype HMAC h = HMAC h deriving (Eq, Storable, CryptoStore)
+
+getHash :: HMAC h -> h
+getHash _ = undefined
+
+instance BlockPrimitive h => BlockPrimitive (HMAC h) where
+
+  blockSize         = blockSize . getHash
+  recommendedBlocks = toEnum . fromEnum . recommendedBlocks . getHash
+
+  newtype Cxt (HMAC h) = HMACCxt (Cxt h)
+
+  process (HMACCxt cxt) blks cptr = HMACCxt <$> process cxt blks' cptr
+          where blks' = toEnum $ fromEnum blks
+
+  processSingle (HMACCxt cxt) cptr = HMACCxt <$> processSingle cxt cptr
+
+
+-- The instance is a straight forward definition from the
+-- corresponding hash. Recall that hmac is computed as follows
+--
+-- > hmac k m = hashByteString $ k2 ++ innerhash
+-- >          where inner = toByteString $ hashByteString k1 m
+-- >
+--
+-- where k1 and k2 are the inner and outer pad respectively each of 1
+-- block length. The HasPadding instance of HMAC has to account for an
+-- additional block of data arising out of the concatination of k1 in
+-- front of the message.
+
+instance HasPadding h => HasPadding (HMAC h) where
+
+  padLength hmac bits = padLength h bits'
+    where h     = getHash hmac
+          bits' = bits + cryptoCoerce (blocksOf 1 hmac)
+
+  padding hmac bits = padding h bits'
+    where h     = getHash hmac
+          bits' = bits + cryptoCoerce (blocksOf 1 hmac)
+
+  unsafePad hmac bits = unsafePad h bits'
+    where h     = getHash hmac
+          bits' = bits + cryptoCoerce (blocksOf 1 hmac)
+
+  maxAdditionalBlocks  = toEnum . fromEnum . maxAdditionalBlocks . getHash
