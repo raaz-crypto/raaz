64b9e49d1e0c794c0ad05acf2d564ec364354540
satvikc <mystic.satvik@gmail.com>
2013-03-13 14:32:17 +0530

    Sha224 and Sha256 structure changed

--- raaz-hash-sha/Raaz/Hash/Sha.hs
+++ raaz-hash-sha/Raaz/Hash/Sha.hs
@@ -8,12 +8,10 @@ sha224, sha512, sha384.
 module Raaz.Hash.Sha
        ( module Raaz.Hash.Sha.Types
        , module Raaz.Hash.Sha1.Type
+       , module Raaz.Hash.Sha256.Type
        ) where
 
-import Raaz.Hash.Sha1.Type
-import Raaz.Hash.Sha.Types( SHA256
-                          , SHA224
-                          , SHA512
-                          , SHA384
-                          )
+import Raaz.Hash.Sha1.Type (SHA1)
+import Raaz.Hash.Sha256.Type (SHA256,SHA224)
+import Raaz.Hash.Sha.Types(SHA512,SHA384)
 import Raaz.Hash.Sha.Instances()
--- raaz-hash-sha/Raaz/Hash/Sha/Instances.hs
+++ raaz-hash-sha/Raaz/Hash/Sha/Instances.hs
@@ -16,65 +16,9 @@ import Foreign.Storable(Storable(..))
 
 import Raaz.Hash
 import Raaz.Hash.Sha.Types
-import Raaz.Hash.Sha.Ref.Sha256
 import Raaz.Hash.Sha.Ref.Sha512
 import Raaz.Primitives
 import Raaz.Types
-
-instance BlockPrimitive SHA224 where
-  blockSize _ = cryptoCoerce $ BITS (512 :: Int)
-  {-# INLINE blockSize #-}
-
-  newtype Cxt SHA224 = SHA224Cxt SHA256
-
-  processSingle (SHA224Cxt cxt) ptr = SHA224Cxt <$> sha256CompressSingle cxt ptr
-
-instance HasPadding SHA224 where
-
-  maxAdditionalBlocks _ = 1
-  padLength = padLength64
-  padding   = padding64
-
-
-instance Hash SHA224 where
-  startHashCxt = SHA224Cxt $ SHA256 0xc1059ed8
-                                    0x367cd507
-                                    0x3070dd17
-                                    0xf70e5939
-                                    0xffc00b31
-                                    0x68581511
-                                    0x64f98fa7
-                                    0xbefa4fa4
-
-  finaliseHash (SHA224Cxt h) = sha256Tosha224 h
-    where sha256Tosha224 (SHA256 h0 h1 h2 h3 h4 h5 h6 _)
-            = SHA224 h0 h1 h2 h3 h4 h5 h6
-
-instance BlockPrimitive SHA256 where
-  blockSize _ = cryptoCoerce $ BITS (512 :: Int)
-  {-# INLINE blockSize #-}
-
-  newtype Cxt SHA256 = SHA256Cxt SHA256
-  processSingle (SHA256Cxt cxt) ptr = SHA256Cxt <$> sha256CompressSingle cxt ptr
-
-
-instance HasPadding SHA256 where
-
-  maxAdditionalBlocks _ = 1
-  padLength = padLength64
-  padding   = padding64
-
-instance Hash SHA256 where
-
-  startHashCxt = SHA256Cxt $ SHA256 0x6a09e667
-                                    0xbb67ae85
-                                    0x3c6ef372
-                                    0xa54ff53a
-                                    0x510e527f
-                                    0x9b05688c
-                                    0x1f83d9ab
-                                    0x5be0cd19
-  finaliseHash (SHA256Cxt h) = h
 
 instance BlockPrimitive SHA512 where
   blockSize _ = cryptoCoerce $ BITS (1024 :: Int)
