10d9406a5591d0111bc706e645471aec0b4d1dd8
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-05-06 13:05:42 +0530

    config:linux handles non-existence of sys files, fixes #42

--- raaz-primitives/Config/Linux.hs
+++ raaz-primitives/Config/Linux.hs
@@ -3,11 +3,12 @@ module Config.Linux
        ( configure
        ) where
 
+import Control.Exception(catch)
 import Config
 import Data.Text(Text, strip, span, unpack)
 import Data.Text.IO(readFile)
 import Data.Char(isDigit)
-import Prelude hiding (readFile, span)
+import Prelude hiding (readFile, span, catch)
 import System.Info(os)
 
 
@@ -24,15 +25,19 @@ configure = do inform "platform is linux"
 -- | Gets the L1 and L2 cache for a linux machine.
 cache :: IO (Int, Int)
 cache = "reading L1 and L2 cache sizes " <:> do
-  l1 <- fmap readCache
-        $ readFile "/sys/devices/system/cpu/cpu0/cache/index1/size"
-  l2 <- fmap readCache
-        $ readFile "/sys/devices/system/cpu/cpu0/cache/index2/size"
+  l1 <- getCache "/sys/devices/system/cpu/cpu0/cache/index1/size"
+  l2 <- getCache "/sys/devices/system/cpu/cpu0/cache/index2/size"
   putStr $ unwords [ "L1 = ", show l1
                    , "L2 = ", show l2
                    ]
   return (l1,l2)
 
+getCache :: FilePath -> IO Int
+getCache fp = fmap readCache (readFile fp) `catch` handler
+    where handler :: IOError -> IO Int
+          handler e = do inform $ show e
+                         return 0
+                 
 readCache :: Text -> Int
 readCache str | unit == "K" = number * 1024
               | unit == "M" = number * 1024 * 1024
