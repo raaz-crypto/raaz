29eb28c3d0eb52ac5acbdd7057d55c65b5825f64
Satvik Chauhan <sc2557@cornell.edu>
2013-05-24 20:31:32 -0400

    Implementation class added with default implementations. Fixes #41

--- raaz-primitives/Raaz/MAC.hs
+++ raaz-primitives/Raaz/MAC.hs
@@ -7,11 +7,14 @@ This module provides the message authentication abstraction
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+
 module Raaz.MAC
-       ( MAC(..)
-       , mac
-       , macByteString, macLazyByteString
-       , macFile
+       ( MACImplementation(..), MAC
+       , mac', mac
+       , macByteString', macByteString
+       , macLazyByteString', macLazyByteString
+       , macFile', macFile
        ) where
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
@@ -32,38 +35,57 @@ import Raaz.Primitives
 -- /do not/ take the lazy option of deriving the @'Eq'@ instance.
 --
 
-class ( BlockPrimitive m
+class ( BlockImplementation i m
       , HasPadding  m
       , Eq          m
       , CryptoStore m
-      ) => MAC m where
+      ) => MACImplementation i m where
 
   -- | The secret key
-  data MACSecret m :: *
+  data MACSecret i m :: *
 
   -- | Convert a bytestring to a secret
-  toMACSecret  :: B.ByteString -> MACSecret m
+  toMACSecret  :: B.ByteString -> MACSecret i m
 
   -- | The starting MAC context
-  startMACCxt  :: MACSecret m -> Cxt m
+  startMACCxt  :: MACSecret i m -> Cxt i m
 
   -- | Finalise the context to a MAC value.
-  finaliseMAC  :: MACSecret m -> Cxt m -> m
+  finaliseMAC  :: MACSecret i m -> Cxt i m -> m
+
+class MACImplementation (DefaultBlockImplementation m) m => MAC m where
 
 -- | Compute the MAC for the byte source.
-mac :: ( ByteSource src
-       , MAC m
-       )
-    => B.ByteString   -- ^ the secret
+mac' :: ( ByteSource src
+        , MACImplementation i m
+        )
+    => i              -- ^ Implementation
+    -> B.ByteString   -- ^ the secret
     -> src            -- ^ the input byte source
     -> IO m
+mac' i secret src =   transformContext cxt0 src
+                  >>= return . finaliseMAC (macsecret i)
+    where macsecret :: MACImplementation i m => i -> MACSecret i m
+          macsecret _ = toMACSecret secret
+          cxt0      = startMACCxt (macsecret i)
 
-mac secret src =   transformContext cxt0 src
-               >>= return . finaliseMAC macsecret
-    where macsecret = toMACSecret secret
-          cxt0      = startMACCxt macsecret
+
+mac :: ( ByteSource src, MAC m)
+    => B.ByteString
+    -> src
+    -> IO m
+mac secret src = go undefined undefined
+  where go :: MAC m => m -> DefaultBlockImplementation m -> IO m
+        go _ i = mac' i secret src
 
 -- | Compute the MAC of a strict bytestring.
+macByteString' :: MACImplementation i m
+               => i            -- ^ Implementation
+               -> B.ByteString -- ^ the secret
+               -> B.ByteString -- ^ the input strict bytestring
+               -> m
+macByteString' i secret = unsafePerformIO . mac' i secret
+
 macByteString :: MAC m
               => B.ByteString -- ^ the secret
               -> B.ByteString -- ^ the input strict bytestring
@@ -71,6 +93,13 @@ macByteString :: MAC m
 macByteString secret = unsafePerformIO . mac secret
 
 -- | Compute the MAC of a lazy bytestring
+macLazyByteString' :: MACImplementation i m
+                   => i            -- ^ Implementation
+                   -> B.ByteString -- ^ the secret
+                   -> L.ByteString -- ^ the input lazy bytestring
+                   -> m
+macLazyByteString' i secret = unsafePerformIO . mac' i secret
+
 macLazyByteString :: MAC m
                   => B.ByteString -- ^ the secret
                   -> L.ByteString -- ^ the input lazy bytestring
@@ -78,12 +107,21 @@ macLazyByteString :: MAC m
 macLazyByteString secret = unsafePerformIO . mac secret
 
 -- | Compute the MAC of a file.
+macFile' :: MACImplementation i m
+         => i            -- ^ Implementation
+         -> B.ByteString -- ^ the secret
+         -> FilePath     -- ^ the input file
+         -> IO m
+macFile' i secret fp =  transformContextFile cxt0 fp
+                  >>= return . finaliseMAC (macsecret i)
+    where macsecret :: MACImplementation i m => i -> MACSecret i m
+          macsecret _ = toMACSecret secret
+          cxt0      = startMACCxt (macsecret i)
+
 macFile :: MAC m
         => B.ByteString -- ^ the secret
         -> FilePath     -- ^ the input file
         -> IO m
-
-macFile secret fp =   transformContextFile cxt0 fp
-                   >>= return . finaliseMAC macsecret
-    where macsecret = toMACSecret secret
-          cxt0      = startMACCxt macsecret
+macFile bs fp = go undefined undefined
+  where go :: MAC m => m -> DefaultBlockImplementation m -> IO m
+        go _ i = macFile' i bs fp
