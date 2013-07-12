0d78717280c9536b2cdf95b57ee2a5e9d4a4b443
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-07-12 18:13:13 +0530

    fixed checksum executable

--- raaz/bin/checksum.hs
+++ raaz/bin/checksum.hs
@@ -11,7 +11,6 @@ that is available on most Unices.
 
 import Control.Applicative ((<$>))
 import qualified Data.ByteString.Char8 as BC
-import System.IO
 import System.Console.CmdArgs
 import Data.Version(showVersion)
 import Paths_raaz(version)
@@ -43,31 +42,21 @@ shasum = cmdArgsMode $ Shasum { hashType = "sha1" &=
 main :: IO ()
 main = do shaArgs <- cmdArgsRun shasum
           case shaArgs of
-            Shasum "sha1"   c fp -> mapM_ (printHashText sha1 c) fp
-            Shasum "sha224" c fp -> mapM_ (printHashText sha224 c) fp
-            Shasum "sha256" c fp -> mapM_ (printHashText sha256 c) fp
-            Shasum "sha384" c fp -> mapM_ (printHashText sha384 c) fp
-            Shasum "sha512" c fp -> mapM_ (printHashText sha512 c) fp
+            Shasum "sha1"   c fp -> mapM_ (printHashText sha1File c) fp
+            Shasum "sha224" c fp -> mapM_ (printHashText sha224File c) fp
+            Shasum "sha256" c fp -> mapM_ (printHashText sha256File c) fp
+            Shasum "sha384" c fp -> mapM_ (printHashText sha384File c) fp
+            Shasum "sha512" c fp -> mapM_ (printHashText sha512File c) fp
             _                  -> error "Unsupported sha hash"
-  where sha1   = undefined :: SHA1
-        sha224 = undefined :: SHA224
-        sha256 = undefined :: SHA256
-        sha384 = undefined :: SHA384
-        sha512 = undefined :: SHA512
 
+printHashText :: Hash h => (FilePath -> IO h) -> Bool -> String -> IO ()
+printHashText hf False fp = do h <- hf fp
+                               BC.putStr $ toHex h
+                               BC.putStr "  "
+                               putStrLn fp
 
-computeHash :: Hash h => String -> IO h
-computeHash "-" = sourceHash stdin
-computeHash fp  = hashFile fp
-
-printHashText :: Hash h => h -> Bool -> String -> IO ()
-printHashText u False fp = do h <- computeHash fp
-                              BC.putStr $ toHex $ asTypeOf h u
-                              BC.putStr "  "
-                              putStrLn fp
-
-printHashText u True fp = do hf <- readHashFile fp
-                             mapM_ (checkAndPrint u) hf
+printHashText hf True fp = do hashes <- readHashFile fp
+                              mapM_ (checkAndPrint hf) hashes
 
 -- | Reads the standard space separate `hash filepath` file and checks
 -- for the correctness of hash of each file.
@@ -78,9 +67,9 @@ readHashFile fp = map readHashLine . lines <$> readFile fp
                        (h:rest) -> (h,unwords rest)
                        _        -> error "illformed hashfile"
 
-checkAndPrint :: Hash h => h -> (String,String) -> IO ()
-checkAndPrint h (ht,fp) = do hs <- computeHash fp
-                             putStr fp
-                             case ((toHex $ hs `asTypeOf` h) == BC.pack ht) of
-                               True  -> putStrLn ": OK"
-                               False -> putStrLn ": FAILED"
+checkAndPrint :: Hash h => (FilePath -> IO h) -> (String,String) -> IO ()
+checkAndPrint hf (ht,fp) = do hs <- hf fp
+                              putStr fp
+                              if toHex hs == BC.pack ht
+                                then putStrLn ": OK"
+                                else putStrLn ": FAILED"
