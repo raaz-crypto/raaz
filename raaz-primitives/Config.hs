e9628f8bc8a3c5e108b5e55d4c10c5b60699680f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-07-19 18:18:33 +0530

    top level config file update

--- raaz-primitives/Config.hs
+++ raaz-primitives/Config.hs
@@ -1,55 +1,29 @@
 module Config
-       ( (<:>), inform
-       , Parameters(..), defaultParameters, toString
-       , define, define'
-       , protectWith
+       ( configure
+       , genConfigFile
        ) where
 
--- | The system parameters.
-data Parameters = Parameters { l1Cache   :: Int
-                                 -- ^ L1 cache in bytes
-                             , l2Cache   :: Int
-                                 -- ^ L2 cache in bytes
-                             , isGCC     :: Bool
-                                 -- ^ GCC (or compatiable) C compiler.
-                             }
+import Config.Monad
+import Config.Cache(cache)
 
-defaultParameters :: Parameters
-defaultParameters = Parameters { l1Cache = 0
-                               , l2Cache = 0
-                               , isGCC   = False
-                               }
+-- | The main configuration action. This justs packages the actual
+-- configuration.
+configure = do
+  section "Auto generated stuff (do not edit)" $
+    wrapHeaderFile "__RAAZ_PRIMITIVES_AUTOCONF_H__" actualConfig
 
-toString :: Parameters -> String
-toString p = unlines [ define "RAAZ_L1_CACHE" $ show $ l1Cache p
-                     , define "RAAZ_L2_CACHE" $ show $ l2Cache p
-                     , if isGCC p then define' "RAAZ_HAVE_GCC"
-                                  else define' "RAAZ_PORTABLE_C"
-                     ]
+-- | Here is where the actual configuration happens.
+actualConfig :: ConfigM ()
+actualConfig = do
+  section "Cache parameters" configureCache
 
+section :: String -> ConfigM () -> ConfigM ()
+section com action = do comment com
+                        action
+                        comment $ "End of " ++ com
+                        newline
 
--- | Define a symbol.
-define :: String -> String -> String
-define symbol value =  unwords ["# define", symbol, value]
-
--- | Empty definition.
-define' :: String -> String
-define' symbol = define symbol ""
-
--- | Protect a content with an ifndef symbol define endif construct.
-protectWith :: String -> String -> String
-protectWith symbol content = unlines [ "# ifndef "  ++ symbol
-                                     , define' symbol
-                                     , content
-                                     , "# endif"
-                                     ]
-
-(<:>)  :: String -> IO a -> IO a
-infixr 0 <:>
-
-(<:>) str action = do putStr $ "    " ++ str ++ " ..."
-                      x <- action
-                      putStrLn " done"
-                      return x
-inform :: String -> IO ()
-inform str = putStrLn $ "    " ++ str 
+-- | Configuring the L1 and L2 cache values.
+configureCache = do (l1,l2) <- cache
+                    define "RAAZ_L1_CACHE" $ show l1
+                    define "RAAZ_L2_CACHE" $ show l2
