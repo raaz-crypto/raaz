17a38358c05b13a7fb65d818991a68002a6d399f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-07-20 13:33:27 +0530

    added ffi testing infrastructure

---
+++ raaz-primitives/Config/FFI.hs
@@ -0,0 +1,63 @@
+{-|
+
+Code to run some ffiTest
+
+-}
+
+module Config.FFI
+       ( ffiTest
+       ) where
+
+import Control.Monad
+import System.Cmd
+import System.Exit
+import System.FilePath
+
+import Config.Monad
+
+-- | Converts exit status to
+status :: ExitCode -> IO Bool
+status  ExitSuccess       = do putStrLn $ "SUCCESS"; return True
+status (ExitFailure code) = do putStrLn $ "FAILED (status code: "
+                                 ++ show code
+                                 ++ ")"
+                               return False
+
+-- | The `ffiTest testDir`, compiles and runs the test located under
+-- the directory testDir. Use this to test out whether a C function is
+-- exposed in your platform and whether it works as exptected.
+--
+-- The directory structure of a test directory is as follows:
+--
+-- testDir: The directory where the source is located
+--
+-- testDir/test.c: The C functions required for the test
+--
+-- testDir/Test.hs: The Haskell code that makes use of the C functions
+--   in test.c/ For more reliable code you may include quickcheck
+--   tests as well
+--
+-- testDir/DESCRIPTION: One line description of what is being tested.
+--
+-- testDir/README: Optional readme for more clarification (mainly for
+--   developers).
+--
+ffiTest :: FilePath -> ConfigM Bool
+ffiTest = doIO . ffiTestIO
+
+-- | The actual IO action.
+ffiTestIO :: FilePath -- ^ test directory
+          -> IO Bool
+ffiTestIO fp = do
+  printDescr
+  cstat <- compile
+  if cstat then do run else return False
+  where c     = fp </> "test.c"
+        hs    = fp </> "Test.hs"
+        test  = fp </> "test"
+        compile    = do putStr "compile:"
+                        status =<< rawSystem "haskell-compiler"
+                          [ "-v0", c, hs, "-o", test]
+        run        = do status =<< rawSystem test []
+        printDescr = do readFile (fp </> "DESCRIPTION") >>= putStr
+                        putStr "..."
