b286e4b4082255c44b3f0f21b5bfc31218e9c9a3
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-07-20 14:01:00 +0530

    added endian function checks

--- raaz-primitives/Config.hs
+++ raaz-primitives/Config.hs
@@ -1,10 +1,15 @@
+
 module Config
        ( configure
        , genConfigFile
        ) where
 
+import System.FilePath
+
 import Config.Monad
+import Config.FFI
 import Config.Cache(cache)
+
 
 -- | The main configuration action. This justs packages the actual
 -- configuration.
@@ -16,6 +21,7 @@ configure = do
 actualConfig :: ConfigM ()
 actualConfig = do
   section "Cache parameters" configureCache
+  section "Endian functions" checkEndian
 
 section :: String -> ConfigM () -> ConfigM ()
 section com action = do comment com
@@ -27,3 +33,15 @@ section com action = do comment com
 configureCache = do (l1,l2) <- cache
                     define "RAAZ_L1_CACHE" $ show l1
                     define "RAAZ_L2_CACHE" $ show l2
+
+-- | Checking for endian conversion functions.
+checkEndian = do haveFFIFunction "htole32"
+                 haveFFIFunction "htole64"
+                 haveFFIFunction "htobe32"
+                 haveFFIFunction "htobe64"
+
+haveFFIFunction :: String -> ConfigM ()
+haveFFIFunction funcName = do chk <- ffiTest ffiPath
+                              if chk then define' $ "RAAZ_HAVE_" ++ funcName
+                                 else undef $ "RAAZ_HAVE_" ++ funcName
+   where ffiPath = "Config" </> "ffi" </> funcName
