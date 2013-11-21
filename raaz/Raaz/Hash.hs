6bbd3cddfed044fa85c2af7c83240432f19dceab
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-11-21 11:04:18 +0530

    documentation for Raaz.Hash module

--- raaz/Raaz/Hash.hs
+++ raaz/Raaz/Hash.hs
@@ -4,6 +4,16 @@ This module exposes all the hash functions provided by the raaz
 library. This is the interface that you would normally want to
 use. You can also import the module for the individual hash function.
 
+Hashing is provided by the generic functions `sourceHash`, `hash`, and
+`hashFile`. The function `sourceHash` can be used with any instance of
+`ByteSource` and it returns the hash wrapped in the IO monad. For
+instances of `PureByteSource`, which include strict and lazy
+`ByteString`, you can use the `hash` function. The `hashFile` hashes a
+given file.  This module also export the specialised variants for
+`sourceHash`, `hash` and `hashFile` for specific hashes. For example,
+in the case of `SHA1` these functions are called `sourceSha1`, `sha1`
+and `sha1File` respectively.
+
 The constructors of the hash types are not exposed here; we expect the
 user to use these types as an opaque type so as to get the benefits of
 type checking. However, if you want to get the constructor for any of
