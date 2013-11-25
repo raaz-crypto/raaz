9f58bff852ba51c8f2ab3e3f4f220a2ab84af0d4
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-11-25 15:28:48 +0530

    moved Raaz.Test.CryptoStore to raaz-primitive

--- raaz-tests/Raaz/Test.hs
+++ raaz-tests/Raaz/Test.hs
@@ -1,8 +1,5 @@
 -- | This module collects all generic tests
 
 module Raaz.Test
-       ( module Raaz.Test.CryptoStore
-       , module Raaz.Test.Hash
+       (
        ) where
-
-import Raaz.Test.CryptoStore
