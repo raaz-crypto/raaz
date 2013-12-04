93a318aff342ef5e4d6d3aa2d688b8e6f7f80661
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-12-04 16:07:18 +0530

    added basic writes

--- raaz-primitives/Raaz/Write.hs
+++ raaz-primitives/Raaz/Write.hs
@@ -4,7 +4,7 @@
 -- location. No range checks are done to speed up the operations and
 -- hence these operations are highly unsafe. Use it with care.
 module Raaz.Write
-       ( Write
+       ( Write, write, writeStorable
        , runWrite
        , runWriteForeignPtr
        ) where
@@ -12,8 +12,11 @@ module Raaz.Write
 import Control.Monad               ( (>=>) )
 import Data.Monoid
 import Foreign.ForeignPtr.Safe     ( withForeignPtr )
+import Foreign.Ptr                 ( castPtr )
+import Foreign.Storable
 
 import Raaz.Types
+import Raaz.Util.Ptr
 
 -- | The write type.
 newtype Write = Write (CryptoPtr -> IO CryptoPtr)
@@ -30,3 +33,20 @@ runWrite cptr (Write action) = action cptr >> return ()
 runWriteForeignPtr   :: ForeignCryptoPtr -> Write -> IO ()
 runWriteForeignPtr fptr (Write action) = withForeignPtr fptr action
                                          >> return ()
+
+-- | Writes a value which is an instance of Storable. This writes the
+-- value machine endian. Mostly it is useful in defining the `poke`
+-- function in a complicated `Storable` instance.
+writeStorable :: Storable a => a -> Write
+writeStorable a = Write $ \ cptr -> do
+  poke (castPtr cptr) a
+  return $ cptr `movePtr` byteSize a
+
+-- | Writes an instance of `CryptoStore`. Endian safety is take into
+-- account here. This is what you would need when you write network
+-- packets for example. You can also use this to define the `load`
+-- function in a compicated `CryptoStore` instance.
+write :: CryptoStore a => a -> Write
+write a = Write $ \ cptr -> do
+  store cptr a
+  return $ cptr `movePtr` byteSize a
