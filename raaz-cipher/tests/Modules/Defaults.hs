16256a6c6b2aae6b3d00b9899f2d3e78a64c5dc4
satvikc <mystic.satvik@gmail.com>
2014-02-28 19:25:34 +0530

    Fixed raaz-cipher to use CGadget and HGadget interface

--- raaz-cipher/tests/Modules/Defaults.hs
+++ raaz-cipher/tests/Modules/Defaults.hs
@@ -59,24 +59,21 @@ testKey256 =  pack [0x60,0x3d,0xeb,0x10
                    ,0x0C,0x0D,0x0E,0x0F]
 
 
-cportableVsReference :: ( CipherGadget g1
-                        , CipherGadget g2
-                        , (PrimitiveOf (g1 Encryption) ~ PrimitiveOf (g2 Encryption))
-                        , (PrimitiveOf (g1 Decryption) ~ PrimitiveOf (g2 Decryption))
-                        , Initializable (PrimitiveOf (g1 Encryption))
-                        , Initializable (PrimitiveOf (g1 Decryption))
-                        , Eq (PrimitiveOf (g1 Encryption))
-                        , Eq (PrimitiveOf (g1 Decryption)))
-                      => (g1 Encryption) -> (g2 Encryption) -> ByteString -> [Test]
+
+cportableVsReference :: ( HasInverse g1
+                        , HasInverse g2
+                        , (PrimitiveOf g1 ~ PrimitiveOf g2)
+                        , (PrimitiveOf (Inverse g1) ~ PrimitiveOf (Inverse g2))
+                        , Initializable (PrimitiveOf g1)
+                        , Initializable (PrimitiveOf (Inverse g1))
+                        , Eq (PrimitiveOf g1)
+                        , Eq (PrimitiveOf (Inverse g1)))
+                     => g1 -> g2 -> ByteString -> [Test]
 cportableVsReference ge1 ge2 iv' =
   [ testGadget ge1 ge2 (getIV iv) "CPortable vs Reference Encryption"
-  , testGadget (gadD ge1) (gadD ge2) (getIV iv) "CPortable vs Reference Decryption"]
+  , testGadget (inverseGadget ge1) (inverseGadget ge2) (getIV iv) "CPortable vs Reference Decryption"]
   where
-    getPrim :: (Gadget (g Encryption)) => g Encryption -> PrimitiveOf (g Encryption)
-    getPrim _ = undefined
-    iv = BS.take (fromIntegral $ ivSize $ getPrim ge1) iv'
-    gadD :: g Encryption -> g Decryption
-    gadD _ = undefined
+    iv = BS.take (fromIntegral $ ivSize $ primitiveOf ge1) iv'
 
 testsDefault m s128 s192 s256 =
       [ testGroup ("AES128 " ++ mode ++ " Reference") $ (testStandardCiphers (pr128 m) s128 "")
@@ -90,16 +87,16 @@ testsDefault m s128 s192 s256 =
       , testGroup ("AES256 " ++ mode ++ " CPortable vs Reference") $ cportableVsReference (pr256 m) (pc256 m) testKey256
       ]
       where
-        pr128 :: m -> Ref128 m Encryption
+        pr128 :: m -> HGadget (Cipher AES KEY128 m Encryption)
         pr128 _ = undefined
-        pr192 :: m -> Ref192 m Encryption
+        pr192 :: m -> HGadget (Cipher AES KEY192 m Encryption)
         pr192 _ = undefined
-        pr256 :: m -> Ref256 m Encryption
+        pr256 :: m -> HGadget (Cipher AES KEY256 m Encryption)
         pr256 _ = undefined
-        pc128 :: m -> CPortable128 m Encryption
+        pc128 :: m -> CGadget (Cipher AES KEY128 m Encryption)
         pc128 _ = undefined
-        pc192 :: m -> CPortable192 m Encryption
+        pc192 :: m -> CGadget (Cipher AES KEY192 m Encryption)
         pc192 _ = undefined
-        pc256 :: m -> CPortable256 m Encryption
+        pc256 :: m -> CGadget (Cipher AES KEY256 m Encryption)
         pc256 _ = undefined
         mode = show $ typeOf m
