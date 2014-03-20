011ce4c4139d11c41e2acc5341b6ded788252aba
satvikc <mystic.satvik@gmail.com>
2014-03-20 17:17:36 +0530

    Added cryptoBuffer which are CryptoPointer with the associated size

--- raaz-primitives/Raaz/Types.hs
+++ raaz-primitives/Raaz/Types.hs
@@ -29,6 +29,7 @@ module Raaz.Types
        , CryptoCoerce(..)
        , cryptoAlignment, CryptoAlign, CryptoPtr
        , ForeignCryptoPtr
+       , CryptoBuffer(..), withCryptoBuffer
        ) where
 
 import Data.Bits
@@ -349,3 +350,12 @@ type ForeignCryptoPtr = ForeignPtr CryptoAlign
 cryptoAlignment :: Int
 cryptoAlignment = alignment (undefined :: CryptoAlign)
 {-# INLINE cryptoAlignment #-}
+
+-- | Pointers with associated size. Reading and writing under the
+-- given size is considered safe.
+data CryptoBuffer = CryptoBuffer {-# UNPACK #-} !(BYTES Int)
+                                 {-# UNPACK #-} !CryptoPtr
+
+-- | Working on the pointer associated with the `CryptoBuffer`.
+withCryptoBuffer :: CryptoBuffer -> (BYTES Int -> CryptoPtr -> IO b) -> IO b
+withCryptoBuffer (CryptoBuffer sz cptr) with = with sz cptr
