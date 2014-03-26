20cbdf9d2b798ff57d438945044898dad894458c
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-03-26 11:28:11 +0530

    documentation of Raaz.Hash improved

--- raaz-hash/Raaz/Hash.hs
+++ raaz-hash/Raaz/Hash.hs
@@ -1,13 +1,7 @@
 {-|
 
 This module exposes all the cryptographic hash functions available
-under the raaz library. You can also import the module for the
-individual hash function for example "Raaz.Hash.Sha1" for the `SHA1`.
-Each of the hash types are to be treated as opaque types as their
-constructors are not exposed from this module. This is to take
-advantage of the type checking. However, if you want to get the
-constructor for the hash type say `SHA1`, you could import the module
-"Raaz.Hash.Sha1.Type".
+under the raaz library.
 
 -}
 
@@ -16,42 +10,52 @@ module Raaz.Hash
          -- * Computing cryptographic hashes.
          -- $computingHash$
          --
-         module Raaz.Hash.Sha1
+         sourceHash, hash, hashFile
+       , toHex, toByteString
+         -- * Exposing individual hashes.
+         -- $individualHashes$
+       , module Raaz.Hash.Sha1
        , module Raaz.Hash.Sha224
        , module Raaz.Hash.Sha256
        , module Raaz.Hash.Sha384
        , module Raaz.Hash.Sha512
-       , sourceHash, hash, hashFile
-       , toHex, toByteString
+
        ) where
 
 
-import Raaz.Hash.Sha1
-import Raaz.Hash.Sha224
-import Raaz.Hash.Sha256
-import Raaz.Hash.Sha384
-import Raaz.Hash.Sha512
-import Raaz.Primitives.Hash ( sourceHash, hash, hashFile )
-import Raaz.Types           ( toByteString               )
-import Raaz.Util.ByteString ( toHex                      )
+import Raaz.Hash.Sha1   hiding ( toByteString, toHex)
+import Raaz.Hash.Sha224 hiding ( toByteString, toHex)
+import Raaz.Hash.Sha256 hiding ( toByteString, toHex)
+import Raaz.Hash.Sha384 hiding ( toByteString, toHex)
+import Raaz.Hash.Sha512 hiding ( toByteString, toHex)
+import Raaz.Primitives.Hash    ( sourceHash, hash, hashFile )
+import Raaz.Types              ( toByteString               )
+import Raaz.Util.ByteString    ( toHex                      )
 
 -- $computingHash$
 --
 -- As opposed to other cryptographic libraries, we capture each
 -- cryptographic hash by a separate type. These types are instances of
--- the type class `Raaz.Primitives.Hash.Hash`. A user should treat
--- them as opaque types for better type safety. In case one wants
--- binary or hexadecimal encoding the functions `toByteString` and
--- `toHex` can be used respectively.
+-- the type class `Raaz.Primitives.Hash.Hash`. Each of the hash types
+-- are to be treated as /opaque types/ as their constructors are not
+-- exposed from this module. This is to take advantage of the type
+-- checking. A cryptographic hash is an instances of the class
+-- `EndianStore`. Therefore, binary or for that matter hexadecimal
+-- encoding can be obtained using the functions `toByteString` and
+-- `toHex` respectively without having access to the constructors.
+-- There is an internal module for each hash type which exposes the
+-- constructors. For example, constructor for the hash `SHA1` is
+-- exposed through the module "Raaz.Hash.Sha1.Internal". However, this
+-- is meant to be used when the standard interfaces provided by raaz
+-- is not sufficient and should be imported in those rare occasions.
 --
 -- There are three functions that you may use to compute the
 -- cryptographic hash. The most generic function for computing a
 -- cryptographic hash is `sourceHash`. The input to this function is
 -- any instance of the class `Raaz.ByteSource.ByteSource` which
 -- includes file `System.IO.Handle`, strict as well as lazy
--- bytestrings and others. The result type is wrapped in an `IO` monad
--- as reading data from certain byte sources like handle can have side
--- effect.
+-- bytestrings. The result type is wrapped in an `IO` monad as reading
+-- data from certain byte sources like handle can have side effect.
 --
 -- > sourceHash :: (Hash hash, ByteSource src) => src -> IO hash
 --
@@ -67,12 +71,20 @@ import Raaz.Util.ByteString ( toHex                      )
 --
 -- > hashFile :: Hash hash => FilePath -> IO hash
 --
--- This module also export the specialised variants for `sourceHash`,
--- `hash` and `hashFile` for specific hashes.  For example, in the
--- case of `SHA1` these functions are called `sourceSha1`, `sha1` and
--- `sha1File` respectively. This is useful in contexts where the type
--- of the hash cannot be infered from the contexts.
+
+
+-- $individualHashes$
+--
+-- Individual hash are exposed via their respective modules.  These
+-- module also export the specialized variants for `sourceHash`,
+-- `hash` and `hashFile` for specific hashes.  For example, if you are
+-- interested only in say `SHA1` you can import the module
+-- "Raaz.Hash.Sha1". This will expose the functions `sourceSha1`,
+-- `sha1` and `sha1File` which are specialized variants of
+-- `sourceHash` `hash` and `hashFile` respectively for the hash
+-- `SHA1`. For example, the sha1 checksum can be computed using the
+-- following code.
 --
 -- > sha1Checksum :: FilePath -> IO ByteString
 -- >            -- compute the  sha1 checksum
--- > sha1DChecksum = sha1File fp >=> return . toHex
+-- > sha1Checksum = fmap toHex . sha1File
