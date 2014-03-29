f32fd88ad5018f60a49c2f8348405a6b7d546463
satvikc <mystic.satvik@gmail.com>
2014-03-29 17:07:33 +0530

    Added testPokePeek

--- raaz-primitives/Raaz/Test/EndianStore.hs
+++ raaz-primitives/Raaz/Test/EndianStore.hs
@@ -8,6 +8,7 @@ using them.
 
 module Raaz.Test.EndianStore
        ( testStoreLoad
+       , testPokePeek
        ) where
 
 import Data.Typeable
@@ -63,4 +64,48 @@ testStoreLoad :: ( EndianStore a
               -> Test
 testStoreLoad a = testProperty (aType ++ ": Store/Load ")
                                $ prop_StoreLoad a
+     where aType = show $ typeOf a
+
+-- | This is where the actual poke/peek is performed.
+pokePeek :: (Storable a, Eq a) => a -> IO Bool
+pokePeek a = allocaBytesAligned (sizeOf a) (alignment a) runPokePeek
+  where runPokePeek ptr = do poke ptr a
+                             y <- peek ptr
+                             return $ y == a
+
+-- | This is the property generator. The first value is an unused
+-- value and is given to satisfy the typechecker.
+prop_PokePeek :: ( Storable a
+                 , Eq a
+                 , Show a
+                 )
+              => a  -- ^ Dummy argument not used
+              -> a  -- ^ load and storing is done on this.
+              -> Property
+prop_PokePeek _ a = monadicIO $ do y <- run $ pokePeek a
+                                   assert y
+
+-- | This test checks whether storing followed by loading gives the
+-- same value for your instance. The typeable instance is used to
+-- print the type when running the test and for nothing else. A
+-- typical use would look something like:
+--
+-- > data Foo = ... deriving (Typeable, Eq, Show)
+-- > instance Storable Foo where
+-- >      ...
+-- > instance Aribitrary Foo where
+-- >      ...
+-- > main = defaultMain [ testPokePeek (undefined :: Foo) ]
+--
+--
+testPokePeek :: ( Storable a
+                , Eq a
+                , Show a
+                , Arbitrary a
+                , Typeable a
+                )
+             => a    -- ^ dummy argument (not used)
+             -> Test
+testPokePeek a = testProperty (aType ++ ": Poke/Peek ")
+                               $ prop_PokePeek a
      where aType = show $ typeOf a
