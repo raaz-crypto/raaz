2dcc4d0639a115849a4e287e03ee5c6e780786fc
gurpreet241092 <gurpreet241092@gmail.com>
2014-05-24 16:00:34 +0530

    Blake256 Module added to Cabal

--- raaz-hash/Raaz/Hash.hs
+++ raaz-hash/Raaz/Hash.hs
@@ -19,18 +19,21 @@ module Raaz.Hash
        , module Raaz.Hash.Sha256
        , module Raaz.Hash.Sha384
        , module Raaz.Hash.Sha512
+       , module Raaz.Hash.Blake256
 
        ) where
-
 
 import Raaz.Hash.Sha1   hiding ( toByteString, toHex)
 import Raaz.Hash.Sha224 hiding ( toByteString, toHex)
 import Raaz.Hash.Sha256 hiding ( toByteString, toHex)
 import Raaz.Hash.Sha384 hiding ( toByteString, toHex)
 import Raaz.Hash.Sha512 hiding ( toByteString, toHex)
+import Raaz.Hash.Blake256
+
 import Raaz.Primitives.Hash    ( sourceHash, hash, hashFile )
 import Raaz.Types              ( toByteString               )
 import Raaz.Util.ByteString    ( toHex                      )
+
 
 -- $computingHash$
 --
