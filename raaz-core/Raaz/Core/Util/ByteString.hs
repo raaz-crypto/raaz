529830516c35292f9fcefef65c91f5544dd39f09
Vinit Kataria <vinitkataria911@gmail.com>
2014-06-24 12:27:05 +0530

    Added functions to convert hexadecimal bytestring to binary bytestring.

--- raaz-core/Raaz/Core/Util/ByteString.hs
+++ raaz-core/Raaz/Core/Util/ByteString.hs
@@ -10,6 +10,7 @@ module Raaz.Core.Util.ByteString
        , unsafeNCopyToCryptoPtr
        , length, replicate
        , hex, toHex
+       , unsafeFromHex, fromHex
        , withByteString
        , fromByteString, fromByteStringStorable
        , createFrom
@@ -99,6 +100,48 @@ hex bs = unsafeCreate (2 * n) filler
                        poke ptr1 $ hexDigit $ bot4 x
             where ptr0 = ptr
                   ptr1 = ptr `plusPtr` 1
+
+-- | Converts hexadecimal bytestring to binary assuming that the input
+--   bytestring is hexadecimal only.
+unsafeFromHex :: ByteString -> ByteString
+unsafeFromHex bs = unsafeCreate (n `div` 2) filler
+  where (fptr, offset, n)      = toForeignPtr bs
+
+        filler ptr = withForeignPtr fptr $
+             \ bsPtr -> putBS (bsPtr `plusPtr` offset) 0 ptr
+
+        putBS bsPtr i ptr
+              | i < (n `div` 2) = do x <- peek  bsPtr
+                                     y <- peek (bsPtr `plusPtr` 1)
+                                     put ptr x y
+                                     putBS bsNewPtr (i+1) ptrNew
+              | otherwise = return ()
+          where bsNewPtr = bsPtr `plusPtr` 2
+                ptrNew   = ptr   `plusPtr` 1
+
+        decDigit x | (x - c2w '0') < 10 = (x - c2w '0')
+                   | otherwise          = 10 + (x - c2w 'a')
+
+        put ptr x y = do poke ptr $ dec
+          where dec = ((decDigit x) `shiftL` 4) .|. (decDigit y)
+
+-- | Converts hexadecimal bytestring to binary. If the input bytestring
+--   is not hexadecimal, returns Nothing.
+fromHex :: ByteString -> Maybe ByteString
+fromHex bs
+    | n == 0 || (n `mod` 2) /= 0 = Nothing
+    | isHexByteString bs 0       = Just (unsafeFromHex bs)
+    | otherwise                  = Nothing
+    where n = B.length bs
+
+          isHexByteString b i
+            | i < n     = (isHex $ B.head b) &&
+                          (isHexByteString (B.tail b) (i+1))
+            | otherwise = True
+
+          isHex x | ((x - c2w '0') >= 0) && ((x - c2w '0') < 10) = True
+                  | ((x - c2w 'a') >= 0) && ((x - c2w 'a') <  6) = True
+                  | otherwise                                    = False
 
 -- | Works directly on the pointer associated with the
 -- `ByteString`. This function should only read and not modify the
