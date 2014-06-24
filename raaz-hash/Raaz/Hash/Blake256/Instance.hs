7521f9c4d043d97bdc1037294d69582077cd79f8
gurpreet241092 <gurpreet241092@gmail.com>
2014-06-24 18:49:31 +0530

    remove roundFloor function calls from Blake Implementation

--- raaz-hash/Raaz/Hash/Blake256/Instance.hs
+++ raaz-hash/Raaz/Hash/Blake256/Instance.hs
@@ -35,47 +35,52 @@ instance Gadget (HGadget BLAKE256) where
   type PrimitiveOf (HGadget BLAKE256) = BLAKE256
   type MemoryOf (HGadget BLAKE256) = (CryptoCell BLAKE256, CryptoCell Salt, CryptoCell (BITS Word64))
   newGadgetWithMemory = return . HGadget
+
   initialize (HGadget (cellBlake, cellSalt, cellCounter)) (BLAKE256Cxt blake salt counter) = do
     cellStore cellSalt salt
     cellStore cellBlake blake
     cellStore cellCounter counter
+
   finalize (HGadget (cellBlake, cellSalt, cellCounter)) = do
     b <- cellLoad cellBlake
     s <- cellLoad cellSalt
     c <- cellLoad cellCounter
     return $ BLAKE256Cxt b s c
+
   apply (HGadget (cellBlake, cellSalt, cellCounter)) n cptr = do
-    initial <- cellLoad cellBlake
-    salt <- cellLoad cellSalt
-    counter <- cellLoad cellCounter
-    (final, nCounter , _ )  <- foldM (moveAndHash salt) (initial, counter, cptr) [1..n]
+    initial                <- cellLoad cellBlake
+    salt                   <- cellLoad cellSalt
+    counter                <- cellLoad cellCounter
+    (final, nCounter , _ ) <- foldM (moveAndHash salt) (initial, counter, cptr) [1..n]
     cellStore cellBlake final
     cellStore cellCounter nCounter
     where
       sz = blockSize (undefined :: BLAKE256)
       moveAndHash salt (cxt, counter, ptr) _ = do
-        let nCounter = counter + roundFloor sz
-        newCxt <- blake256CompressSingle cxt salt nCounter ptr
+        let nCounter = counter + inBits sz
+        newCxt       <- blake256CompressSingle cxt salt nCounter ptr
         return (newCxt, nCounter, ptr `movePtr` sz)
 
 instance PaddableGadget (HGadget BLAKE256) where
-	unsafeApplyLast g@(HGadget (_, _, cellCounter)) blocks bytes cptr = do
-	  let bits = roundFloor bytes :: BITS Word64
-	      len  = roundFloor blocks + bits
-	      p = primitiveOf g
-	      block = blockSize p
-	      padl = padLength p len
-	      padBlocks = roundFloor padl `asTypeOf` blocks
-	      tBlocks = roundFloor (bytes + padl)
-	  unsafePad p len (cptr `movePtr` bytes)
-	  if padBlocks==0
-		  then do
-			apply g (tBlocks-1) cptr
-			cellModify cellCounter (\a -> a - roundFloor padl)
-			apply g 1 (cptr `movePtr` (tBlocks - 1))
-		else do
-			apply g (tBlocks-2) cptr
-			cellModify cellCounter (\a -> a - roundFloor (padl - block))
-			apply g 1 (cptr `movePtr` (tBlocks-2))
-			cellStore cellCounter 0
-			apply g 1 (cptr `movePtr` (tBlocks-1))
+  unsafeApplyLast g@(HGadget (_, _, cellCounter)) blocks bytes cptr = do
+    let bits      = inBits bytes :: BITS Word64
+        len       = inBits blocks + bits
+        p         = primitiveOf g
+        block     = blockSize p
+        padl      = padLength p len
+        padBlocks = atMost padl `asTypeOf` blocks
+        tBlocks   = atLeast (bytes + padl)
+        callApply pblocks
+          | pblocks == 0 = do
+                             apply g (tBlocks-1) cptr
+                             cellModify cellCounter (\a -> a - inBits padl)
+                             apply g 1 (cptr `movePtr` (tBlocks - 1))
+          | otherwise    = do
+                             apply g (tBlocks-2) cptr
+                             cellModify cellCounter (\a -> a - inBits (padl - block))
+                             apply g 1 (cptr `movePtr` (tBlocks-2))
+                             cellStore cellCounter 0
+                             apply g 1 (cptr `movePtr` (tBlocks-1))
+
+    unsafePad p len (cptr `movePtr` bytes)
+    callApply padBlocks
