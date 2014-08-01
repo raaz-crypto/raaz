14693a179874b64442870c6f0242af791c762329
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-08-01 14:21:18 +0530

    documented sha padding.

--- raaz-hash/Raaz/Hash/Sha/Util.hs
+++ raaz-hash/Raaz/Hash/Sha/Util.hs
@@ -13,11 +13,23 @@ import Raaz.Core.Primitives
 import Raaz.Core.Types
 import Raaz.Core.Util.ByteString ( length )
 
-firstPadByte :: Word8
-firstPadByte = 128
+-- The padding used by sha family of hashes is as follows
+--
+-- 1. append a 1 to the bit stream
+--
+-- 2. In the current block if there is enough space for storing the
+-- message length, then append the message length in big endian at the
+-- end of the block and fill the rest with zeros. Otherwise have an
+-- extra block and do the above.
+--
+-- Since we handle messages in bytes instead of bits we invariably
+-- have to append first a byte with 1 as the MSB and follow the above
+-- procedure.
 
--- | This computes the padding length for the sha family of
--- hashes.
+firstPadByte :: Word8
+firstPadByte = 0x80
+
+-- | This computes the padding length for the sha family of hashes.
 shaPadLength :: Primitive prim
           => BYTES Int      -- ^ The bytes need to encode the
                             -- message length
