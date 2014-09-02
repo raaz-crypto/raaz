d1e42f64657ec63766708c4252b715d809e22ade
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-09-02 17:17:55 +0530

    further cleanup of random numbers and got rid of specialize.

--- raaz-random/Raaz/Random/Number.hs
+++ raaz-random/Raaz/Random/Number.hs
@@ -19,7 +19,12 @@ import Raaz.Random.Stream
 
 -- | Generates a positive number i such that 0 <= i <= m. The argument
 -- should always be positive.
-genMax :: (StreamGadget g, Integral i) => RandomSource g -> i -> IO i
+genMax :: ( StreamGadget g
+          , Integral i
+          )
+       => RandomSource g -- ^ the random source
+       -> i              -- ^ the bound
+       -> IO i
 genMax _ 0 = return 0
 genMax rscr maxI | maxI < 0  = error "Illegal arguments"
                  | otherwise = do
@@ -32,24 +37,14 @@ genMax rscr maxI | maxI < 0  = error "Illegal arguments"
     go 0  !m = m
     go !n !m = go (n `quot` 256) (m+1)
 
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> Int -> IO Int #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> Integer  -> IO Integer  #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> Word8    -> IO Word8    #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> BE Word32 -> IO (BE Word32) #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> LE Word32 -> IO (LE Word32) #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> BE Word64 -> IO (BE Word64) #-}
-{-# SPECIALIZE genMax :: StreamGadget g => RandomSource g -> LE Word64 -> IO (LE Word64) #-}
-
-
 -- | Generates a positive number i such that l <= i <= h
-genBetween :: (StreamGadget g,Integral i) => RandomSource g -> i -> i -> IO i
+genBetween :: ( StreamGadget g
+              , Integral i
+              )
+           => RandomSource g  -- ^ The random source
+           -> i               -- ^ starting value
+           -> i               -- ^ ending value
+           -> IO i
 genBetween rsrc l h = do
   n <- genMax rsrc (h-l)
   return (l+n)
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> Int      -> Int      -> IO Int      #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> Integer  -> Integer  -> IO Integer  #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> Word8    -> Word8    -> IO Word8    #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> BE Word32 -> BE Word32 -> IO (BE Word32) #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> LE Word32 -> LE Word32 -> IO (LE Word32) #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> BE Word64 -> BE Word64 -> IO (BE Word64) #-}
-{-# SPECIALIZE genBetween :: StreamGadget g => RandomSource g -> LE Word64 -> LE Word64 -> IO (LE Word64) #-}
