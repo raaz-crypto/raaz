387e5b629ce992ea452b58bf6d0cb70161600c32
Piyush P Kurur <ppk@cse.iitk.ac.in>
2014-10-28 19:01:58 +0530

    sorted module listing in R.Core.hs

--- raaz-core/Raaz/Core.hs
+++ raaz-core/Raaz/Core.hs
@@ -8,22 +8,22 @@ module Raaz.Core
        ( module Raaz.Core.ByteSource
        , module Raaz.Core.Memory
        , module Raaz.Core.Primitives
+       , module Raaz.Core.Primitives.Asymmetric
        , module Raaz.Core.Primitives.HMAC
        , module Raaz.Core.Primitives.Hash
        , module Raaz.Core.Primitives.Cipher
        , module Raaz.Core.Primitives.Symmetric
-       , module Raaz.Core.Primitives.Asymmetric
        , module Raaz.Core.Types
        ) where
 
 import Raaz.Core.ByteSource
 import Raaz.Core.Memory
 import Raaz.Core.Primitives
+import Raaz.Core.Primitives.Asymmetric
 import Raaz.Core.Primitives.HMAC
 import Raaz.Core.Primitives.Hash
 import Raaz.Core.Primitives.Cipher
 import Raaz.Core.Primitives.Symmetric
-import Raaz.Core.Primitives.Asymmetric
 import Raaz.Core.Types
 
 {-# ANN module "HLint: ignore Use import/export shortcut" #-}
