de990ed34588df9312ead57cfe79c08149de0787
abhijaju <abhimanyujaju92@gmail.com>
2014-12-22 22:42:37 +0530

    removed hlint errors from raaz-modular

--- raaz-modular/benchmarks/Modules/Number/Modular.hs
+++ raaz-modular/benchmarks/Modules/Number/Modular.hs
@@ -16,12 +16,12 @@ benchExponentiation p = [ bench "powModuloSlow" $ nf powModuloSlow' p
                         , bench "powModuloSlowSafe-timing" $ nf powModuloSlowSafeTiming' p
                         ]
   where powModuloSlow' (ParamList glist klist mlist glist' klist' mlist') = powModuloSlow'' glist klist mlist
-        powModuloSlow'' [g] [k] [m] = [(powModuloSlow g k m)]
-        powModuloSlow'' (g:gs) (k:ks) (m:ms) = (powModuloSlow g k m) : (powModuloSlow'' gs ks ms)
+        powModuloSlow'' [g] [k] [m] = [powModuloSlow g k m]
+        powModuloSlow'' (g:gs) (k:ks) (m:ms) = powModuloSlow g k m : powModuloSlow'' gs ks ms
         powModuloSlowSafe' (ParamList glist klist mlist glist' klist' mlist') = powModuloSlowSafe'' glist klist mlist
-        powModuloSlowSafe'' [g] [k] [m] = [(powModuloSlowSafe g k m)]
-        powModuloSlowSafe'' (g:gs) (k:ks) (m:ms) = (powModuloSlowSafe g k m) : (powModuloSlowSafeTiming'' gs ks ms)
+        powModuloSlowSafe'' [g] [k] [m] = [powModuloSlowSafe g k m]
+        powModuloSlowSafe'' (g:gs) (k:ks) (m:ms) = powModuloSlowSafe g k m : powModuloSlowSafeTiming'' gs ks ms
         powModuloSlowSafeTiming' (ParamList glist klist mlist glist' klist' mlist') = powModuloSlowSafeTiming'' glist' klist' mlist'
-        powModuloSlowSafeTiming'' [g] [k] [m] = [(powModuloSlowSafe g k m)]
-        powModuloSlowSafeTiming'' (g:gs) (k:ks) (m:ms) = (powModuloSlowSafe g k m) : (powModuloSlowSafeTiming'' gs ks ms)
+        powModuloSlowSafeTiming'' [g] [k] [m] = [powModuloSlowSafe g k m]
+        powModuloSlowSafeTiming'' (g:gs) (k:ks) (m:ms) = powModuloSlowSafe g k m : powModuloSlowSafeTiming'' gs ks ms
 
