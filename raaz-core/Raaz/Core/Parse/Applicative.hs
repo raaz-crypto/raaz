35e835b3f5d049eeee0103b48961f20ca86f80de
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-03-24 15:21:13 +0530

    added parsing of vectors.

--- raaz-core/Raaz/Core/Parse/Applicative.hs
+++ raaz-core/Raaz/Core/Parse/Applicative.hs
@@ -2,23 +2,26 @@
 -- parser which has only an applicative instance.
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Raaz.Core.Parse.Applicative
-       ( Parser, parseWidth
+       ( Parser, parseWidth, parseError
        , runParser, runParser', unsafeRunParser
        , parse, parseStorable
+       , parseVector, parseStorableVector
+       , unsafeParseVector, unsafeParseStorableVector
        , parseByteString
        ) where
 
 import           Data.ByteString           (ByteString)
 import           Data.Maybe                (fromJust)
 import           Data.Monoid               (Sum(..))
+import           Data.Vector.Generic       (Vector, generateM)
 import           Foreign.Ptr               (castPtr)
-import           Foreign.Storable          (Storable, peek)
+import           Foreign.Storable          (Storable, peek, peekElemOff)
 
 
 import           Raaz.Core.MonoidalAction
 import           Raaz.Core.Types
 import           Raaz.Core.Util.ByteString (createFrom)
-import           Raaz.Core.Util.Ptr        (byteSize)
+import           Raaz.Core.Util.Ptr        (byteSize, loadFromIndex)
 
 
 type BytesMonoid = Sum (BYTES Int)
@@ -30,6 +33,10 @@ makeParser :: LengthUnit l => l -> (CryptoPtr -> IO a) -> Parser a
 makeParser l action = TwistRA { twistFieldA       = liftToFieldM action
                               , twistDisplacement = Sum (inBytes l)
                               }
+
+-- | A parser that fails with a given error message.
+parseError  :: String -> Parser a
+parseError msg = makeParser (0 :: BYTES Int) $ \ _ -> fail msg
 
 -- | Return the bytes that this parser will read.
 parseWidth :: Parser a -> BYTES Int
@@ -72,3 +79,42 @@ parse = pa
 -- | Parses a strict bytestring of a given length.
 parseByteString :: LengthUnit l => l -> Parser ByteString
 parseByteString l = makeParser l $ createFrom l
+
+-- | Similar to @parseStorableVector@ but is expected to be slightly
+-- faster. It does not check whether the length parameter is
+-- non-negative and hence is unsafe. Use it only if you can prove that
+-- the length parameter is non-negative.
+unsafeParseStorableVector :: (Storable a, Vector v a) => Int -> Parser (v a)
+unsafeParseStorableVector n = pvec
+  where pvec      = makeParser  width $ \ cptr -> generateM n (getA cptr)
+        width     = fromIntegral n * byteSize (undefA pvec)
+        undefA    :: (Storable a, Vector v a)=> Parser (v a) -> a
+        undefA _  = undefined
+        getA      = peekElemOff . castPtr
+
+-- | Similar to @parseVector@ but is expected to be slightly
+-- faster. It does not check whether the length parameter is
+-- non-negative and hence is unsafe. Use it only if you can prove that
+-- the length parameter is non-negative.
+unsafeParseVector :: (EndianStore a, Vector v a) => Int -> Parser (v a)
+unsafeParseVector n = pvec
+  where pvec     = makeParser  width $ \ cptr -> generateM n (loadFromIndex cptr)
+        width    = fromIntegral n * byteSize (undefA pvec)
+        undefA   :: (EndianStore a, Vector v a)=> Parser (v a) -> a
+        undefA _ = undefined
+
+-- | Similar to `parseVector` but parses according to the host
+-- endian. This function is essentially used to define storable
+-- instances of complicated data. It is unlikely to be of use when
+-- parsing externally serialised data as one would want to keep track
+-- of the endianness of the data.
+parseStorableVector :: (Storable a, Vector v a) => Int -> Parser (v a)
+parseStorableVector n | n < 0      = parseError $ "parseStorableVector on " ++ show n
+                      | otherwise  = unsafeParseStorableVector n
+
+-- | Parses a vector of elements. It takes care of the correct endian
+-- conversion. This is the function to use while parsing external
+-- data.
+parseVector :: (EndianStore a, Vector v a) => Int -> Parser (v a)
+parseVector n | n < 0      = parseError $ "parseVector on " ++ show n
+              | otherwise  = unsafeParseStorableVector n
