f1042e1df464ddf6998bf43e0153d6fa4f6da9a7
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-05-26 15:34:55 +0530

    fields now simplified
    - it is just WrappedArrow
    - got rid of unnecessary right actions.

--- raaz-core/Raaz/Core/MonoidalAction.hs
+++ raaz-core/Raaz/Core/MonoidalAction.hs
@@ -9,17 +9,14 @@ module Raaz.Core.MonoidalAction
          -- $basics$
          LAction (..), Distributive, SemiR (..), (<++>)
          -- ** Monoidal action on functors
-       , LActionF(..), DistributiveF, TwistRF, unTwistRF
+       , LActionF(..), DistributiveF, TwistRF (..), unTwistRF
          -- * Fields
          -- $fields$
        , FieldA, FieldM, Field, computeField, runFieldM, liftToFieldM
-
-       , TwistRA(..), TwistR, TwistRM
        ) where
 
 import Control.Arrow
 import Control.Applicative
-import Control.Category (Category)
 import Data.Monoid
 
 import Raaz.Core.Types    (CryptoPtr, LengthUnit)
@@ -189,18 +186,17 @@ instance DistributiveF m f => Applicative (TwistRF f m) where
 -- @F^g   = λ x -> F (x^g) @
 --
 -- For our applications, we need to define generalised fields
--- associated with arrows (See the type `FieldA`). This is because we
--- often have to deal with functions that have side effects
--- (i.e. `Kleisli` arrows). However, for conceptual understanding, it
--- is sufficient to stick to ordinary functions. In fact, the informal
--- proofs that we have scattered in the source all have been written
--- only for the arrow @->@.
+-- associated with arrows. This is because we often have to deal with
+-- functions that have side effects (i.e. `Kleisli` arrows). However,
+-- for conceptual understanding, it is sufficient to stick to ordinary
+-- functions. In fact, the informal proofs that we have scattered in
+-- the source all have been written only for the arrow @->@.
 
 -- | A field on the space is a function from the points in the space
 -- to some value. Here we define it for a general arrow.
 
-newtype FieldA arrow space value =
-  FieldA { unFieldA :: arrow space value } deriving (Category, Arrow)
+type FieldA arrow = WrappedArrow arrow
+
 
 -- | A field where the underlying arrow is the (->). This is normally
 -- what we call a field.
@@ -208,122 +204,19 @@ type Field = FieldA (->)
 
 -- | Compute the value of a field at a given point in the space.
 computeField :: Field space b -> space -> b
-computeField = unFieldA
+computeField = unwrapArrow
 
 -- | A monadic arrow field.
 type FieldM monad = FieldA (Kleisli monad)
 
 -- | Lift a monadic action to FieldM.
 liftToFieldM :: Monad m => (a -> m b) -> FieldM m a b
-liftToFieldM action = FieldA (Kleisli action)
+liftToFieldM = WrapArrow . Kleisli
 
 -- | Runs a monadic field at a given point in the space.
 runFieldM :: FieldM monad space b -> space -> monad b
-runFieldM = runKleisli . unFieldA
+runFieldM = runKleisli . unwrapArrow
 
-instance Arrow a => Functor (FieldA a space) where
-  fmap f fM = fM >>^ f
-
--- A proof that this is indeed applicative is available in Functional
--- pearl "Applicative programming with effects" Conor McBride and Ross
--- Patterson.
-instance Arrow a => Applicative (FieldA a point) where
-  pure v = arr (const v)
-  (<*>) f x = proc p -> do func <- f -< p
-                           val  <- x -< p
-                           returnA   -< func val
-
--- | A monoidal field is a monoid in itself @f <> g = \ point -> f
--- point <> g point@
-instance (Arrow arrow, Monoid value) => Monoid (FieldA arrow space value) where
-  mempty        =  arr $ const mempty
-  mappend f1 f2 =  proc p -> do n1 <- f1 -< p
-                                n2 <- f2 -< p
-                                returnA -< n1 <> n2
-
-
-
--- | So does left action @(a . f) (x) = f(a . x)@.
-instance (Arrow arrow, LAction m space) => LAction m (FieldA arrow space value) where
-  a <.> f = f <<^ (a<.>) -- first displace the argument and apply the
-                         -- function.
-
--- | On monoidal fields the left action is distributive. Proof: @(m
--- <.> (f <++> g)) = f (m <.> x) <++> g (m <.> x) = ((m <.> f) <> (m
--- <.> g)) (x)@
-instance (Arrow arrow, Monoid value, LAction m space) => Distributive m (FieldA arrow space value)
-
-
--- | The twisted field. This is essentially a field on the space of
--- points tagged with an extra displacement by the monoid. The
--- applicative instance also keeps track of the extra displacement and
--- acts accordingly. This is the applicative generalisation of the
--- semidirect product `SemiR` and in our specific case turns out to
--- capture applicative parsers.
-data TwistRA a space m value = TwistRA { twistFieldA        :: FieldA a space value
-                                       , twistDisplacement  :: m
-                                       }
-
-instance Arrow arrow => Functor (TwistRA arrow space m)  where
-  fmap f (TwistRA fld m) = TwistRA (fmap f fld) m
-
-instance (Monoid m, Arrow arrow, LAction m space) => Applicative (TwistRA arrow space m) where
-  pure x  = TwistRA (pure x) mempty
-  TwistRA f u <*> TwistRA val v = TwistRA (f <*> (u <.> val)) (u <> v)
-
--- | Twisted field when the underlying arrow is @(->)@.
-type TwistR space m = TwistRA (->) space m
-
--- | Monadic twisted field.
-type TwistRM monad space m = TwistRA (Kleisli monad) space m
-
-
-{----------------------------- Right action --------------------------------
-
--- | A (multiplicative) monoid @g@ acting on the right of a space. For
--- right actions it is intuitive to think of it as an exponentiation
--- and the monoid action as a multiplication. The following laws
--- should be true for any right action:
---
--- [@identity law:@]
---       @p ^ 1 = p@
---
--- [@successive displacements:@]
---       @p ^ (a . b) = (p ^ a) ^ b@
-class Monoid m => RAction space m where
-  -- | Apply the monoid on the space
-  (<^>)   ::  space -> m -> space
-
-
--- | A monoid action on a monoidal space is called Monoidal if it
--- satisfies the additional law:
---
--- @(p1 . p2) ^ m = (p1 ^ g) . (p2 ^ g)@.
---
--- It means that for every element @g@ is monoid morphism of the
--- space.
-class (RAction space g, Monoid space) => Monoidal space g
-
-infixl 7 <^>
-
-instance LengthUnit u => RAction CryptoPtr (Sum u) where
-  ptr <^> a  = movePtr ptr (getSum a)
-
--- | The semidirect product Monoid ⋉ Space.
-newtype SemiL m space = SemiL { unSemiL :: (g, space) }
-
-instance Monoidal  space m => Monoid (SemiL m space) where
-  mempty = SemiL (mempty, mempty)
-  mappend (SemiL (a, x)) (SemiL (b, y)) = SemiL (a <> b, x <^> b  <> y)
-
--- | Exponentiation carry over to monoidal fields. @f^a (x) = f
--- (x^a)@.
-instance (Arrow arrow, RAction space g) => RAction (FieldA arrow space value) m where
-  f <^> a  = f <<^ (<^>a) -- first exponentiate the argument and apply
-                          -- the function.
-
--- | Exponentiation on monoidal fields are monoidal. Proof: @ (fg)^a (x) = (fg)(x^a) =
--- f (x^a) . g(x^a) = (f^a . g^a) (x)@
-instance (Arrow arrow, Monoid value, RAction space g) => Monoidal (FieldA arrow space value) g
-
---}
+-- | The action on the space translates to the action on field.
+instance (Arrow arrow, LAction m space) => LActionF m (WrappedArrow arrow space) where
+  m <<.>> field =   WrapArrow $ unwrapArrow field <<^ (m<.>)
