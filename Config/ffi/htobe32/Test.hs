54757127e64c5cafe8015d35ae3930b7c1dc9b67
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-08-25 19:19:51 +0530

    fixed missing file from cabal sdist.

--- Config/ffi/htobe32/Test.hs
+++ Config/ffi/htobe32/Test.hs
@@ -1,13 +1,9 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 import Data.Word
-import System.Endian
-import Test.Framework(defaultMain, Test)
-import Test.Framework.Providers.QuickCheck2(testProperty)
-
+import System.Exit
 foreign import ccall "be32" c_be32 :: Word32 -> Word32
 
-prop x = c_be32 x == toBE32 x
 
 main :: IO ()
-main = defaultMain [testProperty "htobe32" prop]
+main = exitSuccess -- Succeed if it compiles
