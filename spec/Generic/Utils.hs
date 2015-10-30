3d01ce8df9bbf8f11c9f2948213dc13fd74623d6
Piyush P Kurur <ppk@cse.iitk.ac.in>
2015-10-30 16:36:38 +0530

    added utility function for processing with gadgets.

--- spec/Generic/Utils.hs
+++ spec/Generic/Utils.hs
@@ -1,10 +1,28 @@
 module Generic.Utils where
-import Test.Hspec
+
+import           Control.Applicative
+import           Control.Monad
+import qualified Data.ByteString as B
+import           Data.ByteString.Internal
+import           Foreign.Ptr
+import           Test.Hspec
+
+
+import Raaz.Core hiding (length, replicate)
+
 
 with :: key -> (key -> Spec) -> Spec
 with key hmsto = hmsto key
 
 
+transform :: Gadget g => Base16 -> g -> IO Base16
+transform inp g  = encodeByteString <$> create size (action  . castPtr)
+  where size  = B.length src
+        src   = decodeFormat inp
+        bytes = BYTES size
+        action cptr = do
+          void $ fillBytes bytes src cptr
+          apply g (atMost bytes) cptr
 
 shortened :: String -> String
 shortened x | l <= 11    = paddedx
