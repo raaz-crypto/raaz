67a9635ac0102c8e5bc47d5bad3daa8ac38b2bb7
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-02-24 17:36:59 +0530

    expose sha1 and sha256 from Raaz.Hash

--- Raaz/Hash.hs
+++ Raaz/Hash.hs
@@ -19,9 +19,9 @@ module Raaz.Hash
          -- * Exposing individual hashes.
          -- $individualHashes$
 
-       -- , module Raaz.Hash.Sha1
+       , module Raaz.Hash.Sha1
        -- , module Raaz.Hash.Sha224
-       -- , module Raaz.Hash.Sha256
+       , module Raaz.Hash.Sha256
        -- , module Raaz.Hash.Sha384
        -- , module Raaz.Hash.Sha512
        -- , module Raaz.Hash.Blake256
@@ -29,9 +29,9 @@ module Raaz.Hash
        ) where
 
 -- import Raaz.Hash.Blake256
--- import Raaz.Hash.Sha1
+import Raaz.Hash.Sha1
 -- import Raaz.Hash.Sha224
--- import Raaz.Hash.Sha256
+import Raaz.Hash.Sha256
 -- import Raaz.Hash.Sha384
 -- import Raaz.Hash.Sha512
 
