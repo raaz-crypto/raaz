1b357f37e3b077929f85710858914d3549b5bc67
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-02-25 16:20:37 +0530

    updated sha512 and sha384

--- Raaz/Hash/Sha512.hs
+++ Raaz/Hash/Sha512.hs
@@ -1,34 +1,61 @@
- {-|
+{-|
 
-The Sha512 hash.
+This module exposes combinators to compute the SHA512 hash and the
+associated HMAC for some common types.
 
 -}
 
 module Raaz.Hash.Sha512
-       ( SHA512, sha512
-       , sha512File, sourceSha512
-       -- * Encoding as binary/hexadecimal
+       ( -- * The SHA512 cryptographic hash
+         SHA512
+       , sha512, sha512File, sha512Source
+       -- * HMAC computation using SHA512
+       , hmacSha512, hmacSha512File, hmacSha512Source
        ) where
 
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 
-import Raaz.Core.ByteSource
-import Raaz.Core.Primitives.Hash ( sourceHash, hash, hashFile )
+import Raaz.Core
 
-import Raaz.Hash.Sha512.Type     ( SHA512 )
-import Raaz.Hash.Sha512.Instance (        )
+import Raaz.Hash.Internal        ( hashSource, hash, hashFile       )
+import Raaz.Hash.Internal.HMAC   ( hmacSource, hmac, hmacFile, HMAC )
+import Raaz.Hash.Sha512.Internal ( SHA512 )
 
--- | Compute the sha512 hash of the given byte source.
-sourceSha512 :: ByteSource src => src -> IO SHA512
-sourceSha512 = sourceHash
+
+-- | Compute the sha512 hash of an instance of `PureByteSource`. Use
+-- this for computing the sha512 hash of a strict or lazy byte string.
+sha512       :: PureByteSource src => src -> SHA512
+sha512       = hash
+{-# SPECIALIZE sha512 :: B.ByteString -> SHA512 #-}
+{-# SPECIALIZE sha512 :: L.ByteString -> SHA512 #-}
+
 
 -- | Compute the sha512 hash of a file.
 sha512File   :: FilePath -> IO SHA512
 sha512File   = hashFile
 
--- | Compute the sha512 hash of a pure byte source.
-sha512       :: PureByteSource src => src -> SHA512
-sha512       = hash
-{-# SPECIALIZE sha512 :: B.ByteString -> SHA512 #-}
-{-# SPECIALIZE sha512 :: L.ByteString -> SHA512 #-}
+-- | Compute the sha512 hash of a general byte source.
+sha512Source :: ByteSource src => src -> IO SHA512
+sha512Source = hashSource
+
+-- | Compute the message authentication code using hmac-sha512.
+hmacSha512 :: PureByteSource src
+           => Key (HMAC SHA512)  -- ^ Key to use
+           -> src                -- ^ pure source whose hmac is to be
+                                 -- computed
+           -> HMAC SHA512
+hmacSha512 = hmac
+
+-- | Compute the message authentication code for a file.
+hmacSha512File :: Key (HMAC SHA512) -- ^ Key to use
+               -> FilePath          -- ^ File whose hmac is to be computed
+               -> IO (HMAC SHA512)
+hmacSha512File = hmacFile
+
+-- | Compute the message authetication code for a generic byte source.
+hmacSha512Source :: ByteSource src
+                 => Key (HMAC SHA512)
+                 -> src
+                 -> IO (HMAC SHA512)
+hmacSha512Source = hmacSource
--- Raaz/Hash/Sha512/Internal.hs
+++ Raaz/Hash/Sha512/Internal.hs
@@ -1,5 +1,87 @@
-module Raaz.Hash.Sha512.Internal (SHA512(..)) where
+{-# LANGUAGE DeriveDataTypeable         #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE ForeignFunctionInterface   #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# CFILES raaz/hash/sha1/portable.c    #-}
 
-import Raaz.Hash.Sha512.Type
-import Raaz.Hash.Sha512.Instance  ()
-import Raaz.Hash.Sha512.CPortable ()
+module Raaz.Hash.Sha512.Internal ( SHA512(..), cPortable ) where
+
+import           Control.Applicative ( (<$>) )
+import           Data.String
+import qualified Data.Vector.Unboxed                  as VU
+import           Data.Word
+import           Data.Typeable       ( Typeable     )
+import           Foreign.Ptr         ( castPtr      )
+import           Foreign.Storable    ( Storable(..) )
+
+import           Raaz.Core
+import           Raaz.Core.Parse.Applicative
+import           Raaz.Core.Write
+import           Raaz.Hash.Sha.Util
+
+import           Raaz.Hash.Internal
+
+----------------------------- SHA512 ---------------------------------
+
+-- | The Sha512 hash value. Used in implementation of Sha384 as well.
+newtype SHA512 = SHA512 (VU.Vector (BE Word64)) deriving Typeable
+
+-- | Timing independent equality testing for sha512
+instance Eq SHA512 where
+ (==) (SHA512 g) (SHA512 h) = oftenCorrectEqVector g h
+
+instance Storable SHA512 where
+  sizeOf    _ = 8 * sizeOf (undefined :: (BE Word64))
+  alignment _ = alignment  (undefined :: (BE Word64))
+
+  peek = unsafeRunParser sha512parse . castPtr
+    where sha512parse = SHA512 <$> unsafeParseStorableVector 8
+
+  poke ptr (SHA512 v) = unsafeWrite writeSHA512 cptr
+    where writeSHA512 = writeStorableVector v
+          cptr = castPtr ptr
+
+instance EndianStore SHA512 where
+  load = unsafeRunParser $ SHA512 <$> unsafeParseVector 8
+
+  store cptr (SHA512 v) = unsafeWrite writeSHA512 cptr
+    where writeSHA512 = writeVector v
+
+
+instance Encodable SHA512
+
+instance IsString SHA512 where
+  fromString = fromBase16
+
+instance Show SHA512 where
+  show =  showBase16
+
+instance Primitive SHA512 where
+  blockSize _ = BYTES 128
+  type Implementation SHA512 = SomeHashI SHA512
+  recommended  _             = SomeHashI $ cPortable
+
+instance Initialisable (HashMemory SHA512) () where
+  initialise _ = initialise $ SHA512
+                 $ VU.fromList [ 0x6a09e667f3bcc908
+                               , 0xbb67ae8584caa73b
+                               , 0x3c6ef372fe94f82b
+                               , 0xa54ff53a5f1d36f1
+                               , 0x510e527fade682d1
+                               , 0x9b05688c2b3e6c1f
+                               , 0x1f83d9abfb41bd6b
+                               , 0x5be0cd19137e2179
+                               ]
+
+instance Hash SHA512 where
+  additionalPadBlocks _ = 1
+
+------------------- The portable C implementation ------------
+
+cPortable :: HashI SHA512 (HashMemory SHA512)
+cPortable = shaImplementation c_sha512_compress length128Write
+
+foreign import ccall unsafe
+  "raaz/hash/sha512/portable.h raazHashSha512PortableCompress"
+  c_sha512_compress  :: Pointer -> Int -> Pointer -> IO ()
