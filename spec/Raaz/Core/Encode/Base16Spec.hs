da0b08323922922f6edd55f896d9922b7b1a9e40
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-02-28 17:30:35 +0530

    better test for base 16 encoding.

--- spec/Raaz/Core/Encode/Base16Spec.hs
+++ spec/Raaz/Core/Encode/Base16Spec.hs
@@ -11,9 +11,5 @@ spec :: Spec
 spec = do
   prop "unsafeFromByteString . toByteString = id" $ \ (x :: Base16) ->
     unsafeFromByteString (toByteString x) `shouldBe` x
-  let range      = (0x10, 0xff :: Word8)
-      genInRange = choose range
-      in
-   context ("for bytes in the range " ++ show range)
-   $ it "base16 and showHex should match"
-   $ forAll genInRange $ \ (x :: Word8) -> show (encodeByteString $ singleton x :: Base16) == fromString (showHex x "")
+  prop "correctly encodes a 64-bit big endian word." $ \ (w :: Word64) ->
+    (read $ "0x" ++ showBase16 (bigEndian w))  == w
