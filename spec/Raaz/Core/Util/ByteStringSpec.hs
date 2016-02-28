44ba8c836f4ddca48a938b66ca0ea73519b4a40c
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-02-28 17:41:39 +0530

    some cleanup of bytestring specs.

--- spec/Raaz/Core/Util/ByteStringSpec.hs
+++ spec/Raaz/Core/Util/ByteStringSpec.hs
@@ -8,30 +8,26 @@ import Foreign.Ptr
 
 import Raaz.Core as RC
 
-genBS :: Gen ByteString
-genBS = pack <$> arbitrary
-
-genBS' :: Gen (ByteString, Int)
-genBS' = do bs <- genBS
-            l  <- choose (0, B.length bs)
-            return (bs, l)
 
 spec :: Spec
 spec = do context "unsafeCopyToPointer" $
             it "creates the same copy at the input pointer"
-            $ feed genBS $ \ bs -> (==bs) <$> clone bs
+            $ feed arbitrary $ \ bs -> (== bs) <$> clone bs
 
-          context "unsafeNCopyToPointer"
-            $ it "creates the same prefix of at the input pointer"
-            $ feed genBS' $ \ (bs,n) -> (==) (take n bs)
-                                        <$> clonePrefix (bs,n)
+          let gen = do bs <- arbitrary
+                       l  <- choose (0, B.length bs)
+                       return (bs, l)
+              in context "unsafeNCopyToPointer"
+                 $ it "creates the same prefix of at the input pointer"
+                 $ feed gen $ \ (bs,n) -> (==) (take n bs)
+                                          <$> clonePrefix (bs,n)
 
           context "createFrom"
             $ it "reads exactly the same bytes from the byte string pointer"
-            $ feed genBS $ \ bs -> (==bs) <$> readFrom bs
+            $ feed arbitrary $ \ bs -> (==bs) <$> readFrom bs
 
-    where clone bs           = create (B.length bs)
-                               $ RC.unsafeCopyToPointer bs . castPtr
+    where clone bs  = create (B.length bs)
+                      $ RC.unsafeCopyToPointer bs . castPtr
           clonePrefix (bs,n)
             = createAndTrim (B.length bs)
               $ \ cptr -> do RC.unsafeNCopyToPointer (BYTES n) bs
