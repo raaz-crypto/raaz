6498077ac9f7905b360823786b32b9d3488c789d
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-02-29 11:59:51 +0530

    tuples: making it work for base >=4.7

--- Raaz/Core/Types/Tuple.hs
+++ Raaz/Core/Types/Tuple.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP                   #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE KindSignatures        #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
@@ -17,11 +18,17 @@ module Raaz.Core.Types.Tuple
 import           Control.Applicative
 import qualified Data.List           as L
 import           Data.Monoid
+
+#if MIN_VERSION_base(4,7,0)
+import           Data.Proxy
+#endif
+
 import qualified Data.Vector.Unboxed as V
 import           GHC.TypeLits
 import           Foreign.Ptr                 ( castPtr      )
 import           Foreign.Storable            ( Storable(..) )
 import           Prelude hiding              ( length       )
+
 
 import Raaz.Core.Types.Equality
 import Raaz.Core.Types.Endian
@@ -33,31 +40,65 @@ import Raaz.Core.Parse.Applicative
 newtype Tuple (dim :: Nat) a = Tuple { unTuple :: V.Vector a }
                              deriving Show
 
--- | Function that returns the dimension of the tuple. The dimension
--- is calculated without inspecting the tuple and hence the term
--- @`dimension` (undefined :: Tuple 5 Int)@ will evaluate to 5.
-dimension :: (V.Unbox a, SingI dim) => Tuple dim a -> Int
-dimension = withSing dimensionP
-  where dimensionP :: (SingI dim, V.Unbox a)
-                   => Sing dim
-                   -> Tuple dim a
-                   -> Int
-        dimensionP sz _ = fromEnum $ fromSing sz
+instance (V.Unbox a, Equality a) => Equality (Tuple dim a) where
+  eq (Tuple u) (Tuple v) = V.foldl' mappend mempty $ V.zipWith eq u v
+
+-- | Equality checking is timing safe.
+instance (V.Unbox a, Equality a) => Eq (Tuple dim a) where
+  (==) = (===)
+
 
 -- | Function to make the type checker happy
 getA :: Tuple dim a -> a
 getA _ = undefined
 
+-- | Function that returns the dimension of the tuple. The dimension
+-- is calculated without inspecting the tuple and hence the term
+-- @`dimension` (undefined :: Tuple 5 Int)@ will evaluate to 5.
+#if !MIN_VERSION_base(4,7,0)
+dimension  :: (V.Unbox a, SingI dim) => Tuple dim a -> Int
+dimensionP :: (SingI dim, V.Unbox a)
+           => Sing dim
+           -> Tuple dim a
+           -> Int
+dimension       = withSing dimensionP
+dimensionP sz _ = fromEnum $ fromSing sz
+#else
+dimension  :: (V.Unbox a, KnownNat dim) => Tuple dim a -> Int
+dimensionP :: (KnownNat dim, V.Unbox a)
+           => Proxy dim
+           -> Tuple dim a
+           -> Int
+dimensionP sz _ = fromEnum $ natVal sz
+dimension = dimensionP Proxy
+#endif
+
 -- | Get the dimension to parser
-getParseDimension :: (V.Unbox a, SingI dim) => Parser (Tuple dim a) -> Int
+#if !MIN_VERSION_base(4,7,0)
+getParseDimension :: (V.Unbox a, SingI dim)
+                  => Parser (Tuple dim a) -> Int
+getTupFromP       :: (V.Unbox a, SingI dim)
+                  => Parser (Tuple dim a) -> Tuple dim a
+#else
+getParseDimension :: (V.Unbox a, KnownNat dim)
+                  => Parser (Tuple dim a)
+                  -> Int
+getTupFromP   :: (V.Unbox a, KnownNat dim)
+              => Parser (Tuple dim a)
+              -> Tuple dim a
+#endif
+
 getParseDimension = dimension . getTupFromP
-  where getTupFromP   :: (V.Unbox a, SingI dim) =>
-                         Parser (Tuple dim a) -> Tuple dim a
-        getTupFromP _ = undefined
+getTupFromP _     = undefined
 
 
-
-instance (V.Unbox a, Storable a, SingI dim) => Storable (Tuple dim a) where
+#if !MIN_VERSION_base(4,7,0)
+instance (V.Unbox a, Storable a, SingI dim)
+         => Storable (Tuple dim a) where
+#else
+instance (V.Unbox a, Storable a, KnownNat dim)
+         => Storable (Tuple dim a) where
+#endif
   sizeOf tup = dimension tup * sizeOf (getA tup)
   alignment  = alignment . getA
 
@@ -68,9 +109,13 @@ instance (V.Unbox a, Storable a, SingI dim) => Storable (Tuple dim a) where
   poke ptr tup = unsafeWrite writeTup cptr
     where writeTup = writeStorableVector $ unTuple tup
           cptr     = castPtr ptr
-
+#if !MIN_VERSION_base(4,7,0)
 instance (V.Unbox a, EndianStore a, SingI dim)
          => EndianStore (Tuple dim a) where
+#else
+instance (V.Unbox a, EndianStore a, KnownNat dim)
+         => EndianStore (Tuple dim a) where
+#endif
   load = unsafeRunParser $ tupParser
     where tupParser = Tuple <$> unsafeParseVector len
           len       = getParseDimension tupParser
@@ -78,19 +123,14 @@ instance (V.Unbox a, EndianStore a, SingI dim)
   store cptr tup = unsafeWrite writeTup cptr
     where writeTup = writeVector $ unTuple tup
 
-
-instance (V.Unbox a, Equality a) => Equality (Tuple dim a) where
-  eq (Tuple u) (Tuple v) = V.foldl' mappend mempty $ V.zipWith eq u v
-
--- | Equality checking is timing safe.
-instance (V.Unbox a, Equality a) => Eq (Tuple dim a) where
-  (==) = (===)
-
 -- | Construct a tuple out of the list. This function is unsafe and
 -- will result in run time error if the list is not of the correct
 -- dimension.
-
+#if !MIN_VERSION_base(4,7,0)
 unsafeFromList :: (V.Unbox a, SingI dim) => [a] -> Tuple dim a
+#else
+unsafeFromList :: (V.Unbox a, KnownNat dim) => [a] -> Tuple dim a
+#endif
 unsafeFromList xs
   | dimension tup == L.length xs = tup
   | otherwise                    = wrongLengthMesg
@@ -99,8 +139,14 @@ unsafeFromList xs
 
 -- | Computes the initial fragment of a tuple. No length needs to be given
 -- as it is infered from the types.
+#if !MIN_VERSION_base(4,7,0)
 initial :: (V.Unbox a, SingI dim0, SingI dim1)
          => Tuple dim1 a
          -> Tuple dim0 a
+#else
+initial :: (V.Unbox a, KnownNat dim0, KnownNat dim1)
+         => Tuple dim1 a
+         -> Tuple dim0 a
+#endif
 initial tup = tup0
   where tup0 = Tuple $ V.take (dimension tup0) $ unTuple tup
