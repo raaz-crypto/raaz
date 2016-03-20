7d918c8636ec0ffe85e28f5cb1a02ab0802b9760
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-03-20 14:49:04 -0400

    hide the internals of all hashes.

--- spec/Common/Instances.hs
+++ spec/Common/Instances.hs
@@ -5,14 +5,6 @@
 -- | Some common instances that are required by the test cases.
 module Common.Instances where
 
-import Data.Vector.Unboxed         as  V
-import GHC.TypeLits
-import Raaz.Hash.Sha1.Internal
-import Raaz.Hash.Sha224.Internal
-import Raaz.Hash.Sha256.Internal
-import Raaz.Hash.Sha512.Internal
-import Raaz.Hash.Sha384.Internal
-
 import Common.Imports
 
 instance Arbitrary w => Arbitrary (LE w) where
@@ -34,44 +26,27 @@ instance Arbitrary ALIGN where
 instance Arbitrary ByteString where
   arbitrary = pack <$> arbitrary
 
-#if !MIN_VERSION_base(4,7,0)
-instance (V.Unbox a, Arbitrary a, SingI dim)
-         => Arbitrary (Tuple dim a) where
-
-  arbitrary = g
-    where g = fmap unsafeFromList $ vector $ dimension $ gTup g
-          gTup   :: (V.Unbox a, SingI dim)
-                 => Gen (Tuple dim a)
-                 -> Tuple dim a
-          gTup _ = undefined
-#else
-instance (V.Unbox a, Arbitrary a, KnownNat dim)
-         => Arbitrary (Tuple dim a) where
-  arbitrary = g
-    where g = fmap unsafeFromList $ vector $ dimension $ gTup g
-          gTup   :: (V.Unbox a, KnownNat dim)
-                 => Gen (Tuple dim a)
-                 -> Tuple dim a
-          gTup _ = undefined
-#endif
+genEncodable :: (Encodable a, Storable a) => Gen a
+genEncodable = go undefined
+  where go :: (Encodable a, Storable a) => a -> Gen a
+        go x = unsafeFromByteString . pack <$> vector (sizeOf x)
 
 ---------------   Arbitrary instances for Hashes ----------------
 
 instance Arbitrary SHA1 where
-  arbitrary = SHA1   <$> arbitrary
+  arbitrary = genEncodable
 
 instance Arbitrary SHA224 where
-  arbitrary = SHA224 <$> arbitrary
+  arbitrary = genEncodable
 
 instance Arbitrary SHA256 where
-  arbitrary = SHA256 <$> arbitrary
+  arbitrary = genEncodable
 
 instance Arbitrary SHA512 where
-  arbitrary = SHA512 <$> arbitrary
+  arbitrary = genEncodable
 
 instance Arbitrary SHA384 where
-  arbitrary = SHA384 <$> arbitrary
-
+  arbitrary = genEncodable
 
 instance Arbitrary Base16 where
   arbitrary =  (encodeByteString . pack) <$> listOf arbitrary
