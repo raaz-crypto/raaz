f824f0a5bf43e255a65b6040fd233e87e8b29f27
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-03-21 12:30:13 -0400

    initial work towards aes.

--- Raaz/Cipher/AES.hs
+++ Raaz/Cipher/AES.hs
@@ -1,15 +1,8 @@
 module Raaz.Cipher.AES
-       ( module Raaz.Cipher.AES.Type
-       , module Raaz.Core.Primitives
-       , module Raaz.Core.Primitives.Cipher
+       (
        ) where
 
 import Raaz.Cipher.AES.Type
-import Raaz.Core.Primitives
-import Raaz.Core.Primitives.Cipher
-
-import Raaz.Cipher.AES.CTR ()
-import Raaz.Cipher.AES.CBC ()
 -- import Raaz.Cipher.AES.ECB ()
 
 {-# ANN module "HLint: ignore Use import/export shortcut" #-}
--- Raaz/Cipher/AES/Type.hs
+++ Raaz/Cipher/AES/Type.hs
@@ -1,14 +1,76 @@
-module Raaz.Cipher.AES.Type
-       (
-         -- * 128 Bit Key
-         KEY128
-         -- * 192 Bit Key
-       , KEY192
-         -- * 256 Bit Key
-       , KEY256
-         -- * AES data type
-       , AES
-       , AESOp
-       )where
+{-# LANGUAGE DataKinds                        #-}
+{-# LANGUAGE KindSignatures                   #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving       #-}
+{-# LANGUAGE ForeignFunctionInterface         #-}
+{-# LANGUAGE FlexibleInstances                #-}
+{-# LANGUAGE MultiParamTypeClasses            #-}
+{-# LANGUAGE EmptyDataDecls                   #-}
+{-# CFILES raaz/cipher/aes/common/expand.c    #-}
 
-import Raaz.Cipher.AES.Internal
+module Raaz.Cipher.AES.Type
+       ( AES(..), RowMem(..), AESWord, AESTuple, KEY128,
+       ) where
+
+import Data.Word
+import Data.String
+import Foreign.Ptr      (castPtr )
+import Foreign.Storable (Storable, poke)
+import GHC.TypeLits
+
+
+import Raaz.Core
+import Raaz.Cipher.Internal
+
+-- | The AES type.
+data AES (n :: Nat) (mode :: CipherMode) = AES
+
+-- | The basic word used in AES.
+type AESWord    = BE Word32
+
+-- | A tuple of AES words.
+type AESTuple n = Tuple n AESWord
+
+-- | Key used for AES-128
+newtype KEY128 = KEY128 (AESTuple 4) deriving (Storable, EndianStore)
+
+instance Encodable KEY128
+
+instance IsString KEY128 where
+  fromString = fromBase16
+
+instance Show KEY128 where
+  show = showBase16
+
+
+-- | Memory used to store AESWords as rows.
+newtype RowMem (n :: Nat) = RowMem { unRowMem :: MemoryCell (AESTuple n) }
+                          deriving Memory
+
+instance Initialisable (RowMem 44) KEY128 where
+  initialise k = liftSubMT unRowMem $ withCell $ pokeExpandAndTranspose k c_expand128 11
+
+foreign import ccall unsafe
+  "raaz/cipher/aes/common/common.h raazAESExpand128"
+  c_expand128 :: Pointer -> IO ()
+
+foreign import ccall unsafe
+  "raaz/cipher/aes/common/common.h raazAESTranspose"
+  c_transpose :: Int -> Pointer -> IO ()
+
+-- | Poke a key and expand it with the given routine.
+pokeAndExpand :: Storable k
+              => k                   -- ^ key to poke
+              -> (Pointer -> IO ())  -- ^ expansion algorithm
+              -> Pointer             -- ^ buffer pointer.
+              -> IO ()
+pokeAndExpand k expander ptr = poke (castPtr ptr) k >> expander ptr
+
+-- | Poke the key, expand and transpose
+pokeExpandAndTranspose :: Storable k
+                       => k                  -- ^ the key to poke
+                       -> (Pointer -> IO())  -- ^ the expansion algorithm
+                       -> Int                -- ^ number of matrix element to transpose
+                       -> Pointer            -- buffer pointer
+                       -> IO ()
+pokeExpandAndTranspose k expander n ptr = pokeAndExpand k expander ptr
+                                        >> c_transpose n ptr
