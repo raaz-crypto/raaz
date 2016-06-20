9bc4f26792fdf2627830e3b836437b088f3c032e
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-06-20 11:21:30 +0530

    remove some useless modules.

--- raaz-random/Raaz/Random.hs
+++
@@ -1,8 +0,0 @@
-module Raaz.Random
-       ( module Raaz.Random.Stream
-       , module Raaz.Random.Number
-       )
-       where
-
-import           Raaz.Random.Number
-import           Raaz.Random.Stream
