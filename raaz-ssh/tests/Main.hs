fa7a0aac502abc15ce18cf58cfefb470af2b1114
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-06-24 12:33:41 +0530

    got rid of ssh related files

--- raaz-ssh/tests/Main.hs
+++
@@ -1,7 +0,0 @@
-import           Data.Version
-import           Paths_raaz_ssh (version)
-
-pkgName = "raaz-ssh-" ++ showVersion version
-
-main :: IO ()
-main = putStrLn $ "Running tests for " ++ pkgName
