f3527f7a785fbf5ca290d3ca732d68fd67c4a304
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-08-19 16:46:16 +0530

    generalised writes to transfers
    - a generalised pointer transfer infrastructure with write as a
      special case.
    
    - Actions can now live in any instance of MonadIO (instead of merely IO action)

--- Raaz/Core/Transfer.hs
+++ Raaz/Core/Transfer.hs
@@ -4,13 +4,15 @@
 {-# LANGUAGE TypeSynonymInstances       #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
-module Raaz.Core.Write
+module Raaz.Core.Transfer
        ( Write, bytesToWrite, unsafeWrite
        , write, writeStorable, writeVector, writeStorableVector
        , writeBytes, writeByteString, skipWrite
        ) where
 
+import           Control.Monad.IO.Class
 import           Data.ByteString           (ByteString)
 import           Data.String
 import           Data.ByteString.Internal  (unsafeCreate)
@@ -26,63 +28,72 @@ import           Raaz.Core.Types.Pointer
 import           Raaz.Core.Util.ByteString as BU
 import           Raaz.Core.Encode
 
--- | The monoid for write.
-newtype WriteM = WriteM { unWriteM :: IO () }
+-- | The monoid for transfering.
+newtype TransferM m = TransferM { unTransferM :: m () }
 
-instance Monoid WriteM where
-  mempty        = WriteM $ return ()
+instance Monad m => Monoid (TransferM m) where
+  mempty        = TransferM $ return ()
   {-# INLINE mempty #-}
 
-  mappend wa wb = WriteM $ unWriteM wa >> unWriteM wb
+  mappend wa wb = TransferM $ unTransferM wa >> unTransferM wb
   {-# INLINE mappend #-}
 
-  mconcat = WriteM . mapM_ unWriteM
+  mconcat = TransferM . mapM_ unTransferM
   {-# INLINE mconcat #-}
 
--- | A write action is nothing but an IO action that returns () on
+-- | A action that transfers some stuff. It is nothing but an action that returns () on
 -- input a pointer.
-type WriteAction = Pointer -> WriteM
+type TransferAction m = Pointer -> TransferM m
 
-instance LAction (BYTES Int) WriteAction where
-  m <.> action = action . (m<.>)
+instance Monad m => LAction (BYTES Int) (TransferAction m) where
+  offset <.> action = action . (offset<.>)
   {-# INLINE (<.>) #-}
 
-instance Distributive (BYTES Int) WriteAction
+instance Monad m => Distributive (BYTES Int) (TransferAction m)
 
--- | A write is an action which when executed using `runWrite` writes
--- bytes to the input buffer. It is similar to the `WU.Write` type
--- exposed from the "Raaz.Write.Unsafe" module except that it keeps
--- track of the total bytes that would be written to the buffer if the
--- action is run. The `runWrite` action will raise an error if the
--- buffer it is provided with is of size smaller. `Write`s are monoid
--- and hence can be concatnated using the `<>` operator.
-type Write = SemiR WriteAction (BYTES Int)
+type Transfer m = SemiR (TransferAction m) (BYTES Int)
 
--- | Create a write action.
-makeWrite :: LengthUnit u => u -> (Pointer -> IO ()) -> Write
-{-# INLINE makeWrite #-}
-makeWrite sz action = SemiR (WriteM . action) $ inBytes sz
+
+makeTransfer :: LengthUnit u => u -> (Pointer -> m ()) -> Transfer m
+{-# INLINE makeTransfer #-}
+makeTransfer sz action = SemiR (TransferM . action) $ inBytes sz
+
+
+-------------------------- Monoid for writing stuff --------------------------------------
+
+-- | A write is an action which when executed using writes
+-- bytes to its input buffer. `Write`s are monoid and hence can be
+-- concatnated using the `<>` operator.
+newtype WriteM m = WriteM { unWriteM :: Transfer m } deriving Monoid
+
+-- | The default write action.
+type Write = WriteM IO
 
 -- | Returns the bytes that will be written when the write action is performed.
-bytesToWrite :: Write -> BYTES Int
-bytesToWrite = semiRMonoid
+bytesToWrite :: WriteM m -> BYTES Int
+bytesToWrite = semiRMonoid . unWriteM
 
 -- | Perform the write action without any checks.
-unsafeWrite :: Write -> Pointer -> IO ()
-unsafeWrite wr =  unWriteM . semiRSpace wr
+unsafeWrite :: WriteM m -> Pointer -> m ()
+unsafeWrite wr =  unTransferM . semiRSpace (unWriteM wr)
 
 {-
 -- | The function tries to write the given `Write` action on the
 -- buffer and returns `True` if successful.
-tryWriting :: Write         -- ^ The write action.
+tryWriting :: WriteM         -- ^ The write action.
            -> CryptoBuffer  -- ^ The buffer to which the bytes are to
                             -- be written.
            -> IO Bool
 tryWriting wr cbuf = withCryptoBuffer cbuf $ \ sz cptr ->
-  if sz < bytesToWrite wr then return False
+  if sz < bytesToWriteM wr then return False
   else do unsafeWrite wr cptr; return True
 
 -}
+
+
+makeWrite     :: LengthUnit u => u -> (Pointer -> m ()) -> WriteM m
+makeWrite sz  = WriteM . makeTransfer sz
+
 
 -- | The expression @`writeStorable` a@ gives a write action that
 -- stores a value @a@ in machine endian. The type of the value @a@ has
@@ -90,47 +101,58 @@ tryWriting wr cbuf = withCryptoBuffer cbuf $ \ sz cptr ->
 -- to talk with C functions and not when talking to the outside world
 -- (otherwise this could lead to endian confusion). To take care of
 -- endianness use the `write` combinator.
-writeStorable :: Storable a => a -> Write
-writeStorable a = makeWrite (byteSize a) pokeIt
-  where pokeIt = flip poke a . castPtr
+writeStorable :: (MonadIO m, Storable a) => a -> WriteM m
+writeStorable a = WriteM $ makeTransfer (byteSize a) pokeIt
+  where pokeIt = liftIO . flip poke a . castPtr
 -- | The expression @`write` a@ gives a write action that stores a
 -- value @a@. One needs the type of the value @a@ to be an instance of
 -- `EndianStore`. Proper endian conversion is done irrespective of
 -- what the machine endianness is. The man use of this write is to
 -- serialize data for the consumption of the outside world.
-write :: EndianStore a => a -> Write
-write a = makeWrite (byteSize a) $ flip store a
+write :: (MonadIO m, EndianStore a) => a -> WriteM m
+write a = makeWrite (byteSize a) $ liftIO . flip store a
 
 -- | The vector version of `writeStorable`.
-writeStorableVector :: (Storable a, G.Vector v a) => v a -> Write
+writeStorableVector :: (Storable a, G.Vector v a, MonadIO m) => v a -> WriteM m
 {-# INLINE writeStorableVector #-}
 writeStorableVector = G.foldl' foldFunc mempty
   where foldFunc w a =  w <> writeStorable a
 
+{-
+
+TODO: This function can be slow due to the fact that each time we use
+the semi-direct product, we incur a cost due to the lambda being not
+lifted.
+
+-}
+
 -- | The vector version of `write`.
-writeVector :: (EndianStore a, G.Vector v a) => v a -> Write
+writeVector :: (EndianStore a, G.Vector v a, MonadIO m) => v a -> WriteM m
 {-# INLINE writeVector #-}
+{-# TODO: improve this using the fact that the size is known #-}
 writeVector = G.foldl' foldFunc mempty
   where foldFunc w a =  w <> write a
+{- TODO: Same as in writeStorableVector -}
+
 
 -- | The combinator @writeBytes n b@ writes @b@ as the next @n@
 -- consecutive bytes.
-writeBytes :: LengthUnit n => Word8 -> n -> Write
+writeBytes :: (LengthUnit n, MonadIO m) => Word8 -> n -> WriteM m
 writeBytes w8 n = makeWrite n memsetIt
-  where memsetIt cptr = memset cptr w8 n
+  where memsetIt cptr = liftIO $ memset cptr w8 n
 
 -- | Writes a strict bytestring.
-writeByteString :: ByteString -> Write
-writeByteString bs = makeWrite (BU.length bs) $  BU.unsafeCopyToPointer bs
+writeByteString :: MonadIO m => ByteString -> WriteM m
+writeByteString bs = makeWrite (BU.length bs) $ liftIO  . BU.unsafeCopyToPointer bs
 
 -- | A write action that just skips over the given bytes.
-skipWrite :: LengthUnit u => u -> Write
+skipWrite :: (LengthUnit u, Monad m) => u -> WriteM m
 skipWrite = flip makeWrite $ const $ return ()
 
-instance IsString Write where
+instance MonadIO m => IsString (WriteM m)  where
   fromString = writeByteString . fromString
 
-instance Encodable Write where
+instance Encodable (WriteM IO) where
   {-# INLINE toByteString #-}
   toByteString w  = unsafeCreate n $ unsafeWrite w . castPtr
     where BYTES n = bytesToWrite w
