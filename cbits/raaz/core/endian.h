7fd99e9d68056a8d5ae065c3e5f2014f6d8f5bc3
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-09-02 16:20:41 +0530

    reworked the endian c functions
    - expose functions to swap bytes
    
    - get rid of endian store function

--- cbits/raaz/core/endian.h
+++ cbits/raaz/core/endian.h
@@ -2,13 +2,6 @@
 #define __RAAZ_ENDIAN_H_
 #include <stdint.h>
 
-/* Include the right header file based on the platform */
-#ifdef PLATFORM_LINUX
-#include <endian.h>
-#elif defined(PLATFORM_BSD) || defined(PLATFORM_OPENBSD)
-#include <sys/endian.h>
-#endif
-
 /* Loads */
 extern uint32_t raazLoadLE32(uint32_t *);
 extern uint32_t raazLoadBE32(uint32_t *);
@@ -16,9 +9,10 @@ extern uint64_t raazLoadLE64(uint64_t *);
 extern uint64_t raazLoadBE64(uint64_t *);
 
 /* Stores */
+/*
 extern void raazStoreLE32(uint32_t *, uint32_t);
 extern void raazStoreBE32(uint32_t *, uint32_t);
 extern void raazStoreLE64(uint64_t *, uint64_t);
 extern void raazStoreBE64(uint64_t *, uint64_t);
-
+*/
 #endif
