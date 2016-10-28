517a6c5f4f6e02c41f6ad5a502cdb63e9a204411
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-10-28 18:05:29 +0530

    included  types for stream cipher.

--- Raaz/Cipher/Internal.hs
+++ Raaz/Cipher/Internal.hs
@@ -17,6 +17,10 @@ module Raaz.Cipher.Internal
          Cipher, CipherMode(..)
          -- ** Cipher implementation
        , CipherI(..), SomeCipherI(..)
+
+         -- ** Stream ciphers.
+         -- $streamcipher$
+       , StreamCipher, makeCipherI
        --
        -- ** Unsafe encryption and decryption.
        -- $unsafecipher$
@@ -34,14 +38,15 @@ import Raaz.Core.Util.ByteString as B
 -- $cipherdoc$
 --
 -- Ciphers provide symmetric encryption in the raaz library and are
--- captured by the type class `Cipher`. Instances of `Cipher` are full
--- encryption/decryption algorithms. For a block cipher this means
--- that one also needs to specify the `CipherMode` to make it an
--- instance of the class `Cipher`. They are instances of the class
--- `Symmetric` and the associated type `Key` captures the encryption
--- key for the cipher.
+-- captured by the type class `Cipher`.  They are instances of the
+-- class `Symmetric` and the associated type `Key` captures the all
+-- that is required to determine the encryption and decryption
+-- process. In most ciphers, this includes what is know as the
+-- _encryption key_ as well as the _initialisation vector_.
 --
--- Implementations of ciphers are captured by two types.
+-- Instances of `Cipher` is only required to provide full block
+-- encryption/decryption algorithms.  Implementations are captured by
+-- two types.
 --
 -- [`CipherI`:] Values of this type that captures implementations of a
 -- cipher.  This type is parameterised over the memory element that is
@@ -65,6 +70,17 @@ import Raaz.Core.Util.ByteString as B
 --    type class `Raaz.Core.Primitives.Recommendation`
 --
 
+-- $streamcipher$
+--
+-- Stream ciphers are special class of ciphers which can encrypt
+-- messages of any length (not necessarily multiples of block length).
+-- Typically, stream ciphers are obtained by xoring the data with a
+-- stream of prg values that the stream ciphers generate. As a
+-- consequence, the encryption and decryption is the same algorithm.
+-- one can also use the stream cipher as a pseudo-random generator.
+--
+-- We have the class `StreamCipher` that captures valid stream ciphers.
+--
 
 
 -- | Block cipher modes.
@@ -82,6 +98,17 @@ data CipherI cipher encMem decMem = CipherI
      , decryptBlocks :: Pointer -> BLOCKS cipher -> MT decMem ()
      }
 
+-- | Type constraints on the memory of a block cipher implementation.
+type CipherM cipher encMem decMem = ( Initialisable encMem (Key cipher)
+                                    , Initialisable decMem (Key cipher)
+                                    ) -- TODO: More need initialisable from buffer.
+
+-- | Some implementation of a block cipher. This type is existentially
+-- quantifies over the memory used in the implementation.
+data SomeCipherI cipher =
+  forall encMem decMem . CipherM cipher encMem decMem
+  => SomeCipherI (CipherI cipher encMem decMem)
+
 instance Describable (CipherI cipher encMem decMem) where
   name        = cipherIName
   description = cipherIDescription
@@ -92,18 +119,41 @@ instance Describable (SomeCipherI cipher) where
   description  (SomeCipherI cI) = description cI
 
 
-type CipherM cipher encMem decMem = ( Initialisable encMem (Key cipher)
-                                    , Initialisable decMem (Key cipher)
-                                    )
-
--- | Some implementation of a block cipher. This type existentially
--- quantifies over the memory used in the implementation.
-data SomeCipherI cipher =
-  forall encMem decMem . CipherM cipher encMem decMem
-  => SomeCipherI (CipherI cipher encMem decMem)
-
+-- | Class capturing ciphers. The implementation of this class should
+-- give an encryption and decryption algorithm for messages of length
+-- which is a multiple of the block size.  Needless to say, the
+-- encryption and decryption should be inverses of each other for such
+-- messages.
 class (Symmetric cipher, Implementation cipher ~ SomeCipherI cipher)
       => Cipher cipher
+
+-- | Class that captures stream ciphers. An instance of `StreamCipher`
+-- should be an instance of `Cipher`, with the following additional
+-- constraints.
+--
+-- 1. The encryption and decryption should be the same algorithm.
+--
+-- 2. Encryption/decryption can be applied to a messages of length @l@
+--    even if @l@ is not a multiple of block length.
+--
+-- 3. The encryption of a prefix of a length @l@ of a message @m@
+--    should be the same as the @l@ length prefix of the encryption of
+--    @m@.
+--
+-- It is the duty of the implementer of the cipher to ensure that the
+-- above conditions are true before declaring an instance of a stream
+-- cipher.
+class Cipher cipher => StreamCipher cipher
+
+
+-- | Constructs a `CipherI`  value out of a stream transformation function. Useful in
+--   building a Cipher instance of a stream cipher.
+makeCipherI :: Primitive prim
+            => String                                -- ^ name
+            -> String                                -- ^ description
+            -> (Pointer -> BLOCKS prim -> MT mem ()) -- ^ stream transformer
+            -> CipherI prim mem mem
+makeCipherI nm des trans = CipherI nm des trans trans
 
 ------------------ Unsafe cipher operations ------------------------
 
