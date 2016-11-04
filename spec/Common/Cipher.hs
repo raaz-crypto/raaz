f1a62e6414b81143746f72acf5c8a072add1d23e
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-11-04 15:51:07 +0530

    generic tests for ciphers: now supports arbitrary implementations.

--- spec/Common/Cipher.hs
+++ spec/Common/Cipher.hs
@@ -10,12 +10,19 @@ encryptVsDecrypt :: ( Arbitrary (Key c)
                     , Cipher c, Recommendation c
                     )
                  => c -> Spec
-encryptVsDecrypt c = describe "decrypt . encrypt" $ do
+encryptVsDecrypt c = encryptVsDecrypt' c $ recommended c
+
+encryptVsDecrypt' :: ( Arbitrary (Key c)
+                     , Show (Key c)
+                     , Cipher c
+                     )
+                     => c -> Implementation c -> Spec
+
+encryptVsDecrypt' c imp = describe "decrypt . encrypt" $ do
   it "trivial on strings of a length that is a multiple of the block size"
     $ property $ forAll genKeyStr prop_EvsD
   where genKeyStr = (,) <$> arbitrary <*> blocks c
-        prop_EvsD (k,bs) = unsafeDecrypt c k (unsafeEncrypt c k bs) == bs
-
+        prop_EvsD (k,bs) = unsafeDecrypt' c imp k (unsafeEncrypt' c imp k bs) == bs
 
 encryptsTo :: (Cipher c, Recommendation c, Format fmt1, Format fmt2)
            => c
@@ -23,9 +30,19 @@ encryptsTo :: (Cipher c, Recommendation c, Format fmt1, Format fmt2)
            -> fmt2
            -> Key c
            -> Spec
-encryptsTo c inp expected key
+
+encryptsTo c = encryptsTo' c $ recommended c
+
+encryptsTo' :: (Cipher c, Format fmt1, Format fmt2)
+            => c
+            -> Implementation c
+            -> fmt1
+            -> fmt2
+            -> Key c
+            -> Spec
+encryptsTo' c imp inp expected key
   = it msg $ result `shouldBe` (decodeFormat expected)
-  where result = unsafeEncrypt c key $ decodeFormat inp
+  where result = unsafeEncrypt' c imp key $ decodeFormat inp
         msg   = unwords [ "encrypts"
                         , shortened $ show inp
                         , "to"
@@ -38,9 +55,19 @@ transformsTo :: (StreamCipher c, Recommendation c, Format fmt1, Format fmt2)
               -> fmt2
               -> Key c
               -> Spec
-transformsTo c inp expected key
+transformsTo c = transformsTo' c $ recommended c
+
+transformsTo' :: (StreamCipher c, Format fmt1, Format fmt2)
+              => c
+              -> Implementation c
+              -> fmt1
+              -> fmt2
+              -> Key c
+              -> Spec
+
+transformsTo' c impl inp expected key
   = it msg $ result `shouldBe` (decodeFormat expected)
-  where result = transform c key $ decodeFormat inp
+  where result = transform' c impl key $ decodeFormat inp
         msg   = unwords [ "encrypts"
                         , shortened $ show inp
                         , "to"
--- spec/Common/Imports.hs
+++ spec/Common/Imports.hs
@@ -16,4 +16,4 @@ import Test.QuickCheck.Monadic as E
 import Raaz.Core               as E hiding ((===), Result)
 import Raaz.Hash               as E
 import Raaz.Cipher             as E
-import Raaz.Cipher.Internal    as E ( unsafeEncrypt, unsafeDecrypt )
+import Raaz.Cipher.Internal    as E ( unsafeEncrypt', unsafeDecrypt', transform' )
