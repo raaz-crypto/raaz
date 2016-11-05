945c5bf6a6cdd011b99e974f1f9d52af794eb327
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-11-05 13:48:35 +0530

    added conditional benchmarking of chacha vector implementation.

--- benchmarks/Cipher.hs
+++ benchmarks/Cipher.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE CPP                 #-}
 import           Control.Monad
 import           Criterion
 import           Criterion.Main
@@ -10,7 +11,9 @@ import           Raaz.Core
 import           Raaz.Cipher
 import           Raaz.Cipher.Internal
 import qualified Raaz.Cipher.ChaCha20.Implementation.CPortable as CPortable
+# ifdef HAVE_VECTOR
 import qualified Raaz.Cipher.ChaCha20.Implementation.GCCVector as GCCVector
+# endif
 
 -- | Buffer size used
 bufSize :: BYTES Int
@@ -35,7 +38,9 @@ aesBench ptr = bgroup "AES"
 chacha20Bench :: Pointer -> Benchmark
 chacha20Bench ptr = bgroup "ChaCha20"
                     [ benchEncrypt' chacha20 CPortable.implementation ptr
+# ifdef HAVE_VECTOR
                     , benchEncrypt' chacha20 GCCVector.implementation ptr
+# endif
                     ]
 
 
@@ -53,8 +58,6 @@ benchDecrypt c = benchDecrypt' c $ recommended c
 benchEncrypt' :: Cipher c => c -> Implementation c -> Pointer -> Benchmark
 benchEncrypt' c si@(SomeCipherI imp) ptr = bench nm $ nfIO $ insecurely $ encryptBlocks imp ptr (atMost bufSize)
   where nm = "encrypt" ++ name si
-
-
 
 benchDecrypt' :: Cipher c => c -> Implementation c -> Pointer -> Benchmark
 benchDecrypt' c si@(SomeCipherI imp) ptr = bench nm $ nfIO $ insecurely $ decryptBlocks imp ptr (atMost bufSize)
