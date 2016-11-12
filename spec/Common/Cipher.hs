ea75f44ce7dc5c076fe89a587d3541b5cda970e7
Piyush P Kurur <ppk@cse.iitk.ac.in>
2016-11-12 17:53:57 +0530

    added tests to cross check implementations.
    - checks a given implementation against the recommended
      implementation.

--- spec/Common/Cipher.hs
+++ spec/Common/Cipher.hs
@@ -32,6 +32,22 @@ encryptsTo :: (Cipher c, Recommendation c, Format fmt1, Format fmt2)
            -> Key c
            -> Spec
 
+crossCheck :: ( Arbitrary (Key c)
+              , Show (Key c)
+              , Cipher c
+              , Recommendation c
+              )
+              => c -> Implementation c -> Spec
+crossCheck c impl = describe mesg $ do
+  it "encryption" $ property $ forAll genKeyStr prop_Enc
+  it "decryption" $ property $ forAll genKeyStr prop_Dec
+  where mesg      = unwords ["cross check with ", name reco , "(recommended implementation)" ]
+        reco      = recommended c
+        genKeyStr = (,) <$> arbitrary <*> blocks c
+        prop_Enc (k,bs) = unsafeEncrypt' c reco k bs == unsafeEncrypt' c impl k bs
+        prop_Dec (k,bs) = unsafeDecrypt' c reco k bs == unsafeDecrypt' c impl k bs
+
+
 encryptsTo c = encryptsTo' c $ recommended c
 
 encryptsTo' :: (Cipher c, Format fmt1, Format fmt2)
