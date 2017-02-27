c965df40b7f68ab8ca595b9a430dfdd28f9b56c7
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-02-27 11:28:37 +0530

    some helpful remarks on ciphers.

--- Raaz/Cipher.hs
+++ Raaz/Cipher.hs
@@ -4,10 +4,10 @@
 module Raaz.Cipher
        ( -- * Ciphers
          -- $cipherdoc$
-         Cipher
+         StreamCipher
+       , transform, chacha20
+       , Cipher
        , aes128cbc, aes192cbc, aes256cbc
-       , StreamCipher
-       , chacha20, transform
        ) where
 
 
@@ -19,4 +19,15 @@ import Raaz.Cipher.Internal (transform, Cipher, StreamCipher)
 --
 -- The raaz library exposes symmetric key encryption using instances
 -- of the class `Cipher`. For a cipher @c@, the type family @`Key` c@
--- gives the type of its key.
+-- gives the type of its key. As of now, we only support the safe
+-- usage of stream ciphers. Encryption and Decryption are the same for
+-- stream ciphers and we call this combinator `transform`. Block
+-- ciphers do not have a natural way to handle streams that are of
+-- size less than their block size. A future release will handle these
+-- issues.
+--
+-- If you are thinking of encryption using private keys consider
+-- encrypted-authenticated modes. Currently we do not have support for
+-- this either but hopefully this will also be fixed soon.
+--
+-- TODO: Fix the above documentation when it is done.
