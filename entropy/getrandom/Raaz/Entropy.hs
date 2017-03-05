ba2e5cf6fd37b3ac2052cc0c796653aca1dbf8a1
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-03-05 19:54:49 +0530

    flag for selecting getrandom for system entropy on linux.

---
+++ entropy/getrandom/Raaz/Entropy.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ForeignFunctionInterface         #-}
+module Raaz.Entropy( getEntropy ) where
+
+import Control.Monad.IO.Class(MonadIO, liftIO)
+import Raaz.Core.Types
+
+-- | The getrandom system call.
+foreign import ccall unsafe
+  "getrandom"
+  c_getrandom :: Pointer      -- Message
+              -> BYTES Int    -- number of bytes to be read.
+              -> Int          -- flags
+              -> IO (BYTES Int)
+
+-- | Get random bytes from using the @getrandom@ system call on
+-- linux. This is only used to seed the PRG and not intended for call
+-- by others.
+getEntropy :: (MonadIO m, LengthUnit l) => l -> Pointer -> m (BYTES Int)
+getEntropy l ptr = liftIO $ c_getrandom ptr lenBytes 0
+  where lenBytes = inBytes l
