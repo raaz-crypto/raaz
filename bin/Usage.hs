ebd9d13f8834aa372beca9d7ea30510b1826370f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-03-21 08:59:59 +0530

    refactor usage and error bailout commands.

---
+++ bin/Usage.hs
@@ -0,0 +1,25 @@
+module Usage( usage, errorBailout ) where
+
+import System.Console.GetOpt
+import System.IO
+import System.Exit
+
+-- | The usage message for the program.
+usage :: [OptDescr a]  -- ^ options
+      -> String        -- ^ Header
+      -> [String]      -- ^ errors
+      -> String
+usage options header errs
+  | null errs = usageInfo header options
+  | otherwise = "raaz: " ++ unlines errs ++ usageInfo header options
+
+
+
+-- | Bail out on error
+errorBailout :: [OptDescr a] -- ^ Options
+             -> String       -- ^ Header
+             -> [String]     -- ^ Errors
+             -> IO b
+errorBailout opts str errs = do
+  hPutStrLn stderr $ usage opts str errs
+  exitFailure
