d4f9a2e753c1009ff805cf1438b2431a4c9ade08
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-18 17:05:01 +0530

    add tests for concatenated sources.

--- spec/Raaz/Core/ByteSourceSpec.hs
+++ spec/Raaz/Core/ByteSourceSpec.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE CPP #-}
 module Raaz.Core.ByteSourceSpec where
 
-import Common
-import System.IO
-import System.IO.Unsafe
+import qualified Data.ByteString as B
+import           System.IO
+import           System.IO.Unsafe
+
+import           Common
+
 
 readResult :: ByteSource src => BYTES Int -> src -> IO (FillResult src)
 readResult n src = allocaBuffer n $ fillBytes n src
@@ -25,3 +28,9 @@ spec :: Spec
 spec = do
   describe "/dev/null" $
     it "should return Exhausted even for a read request of 0" devNullExpectation
+
+  let genL :: [ByteString] ->  Gen (BYTES Int)
+      genL bs =  fromIntegral <$> choose (0, sum $ map B.length bs)
+    in describe "concatenated source" $
+       prop "reading from a list is equivalent to reading from concatenation" $
+       \ bs -> forAll (genL bs) $ \ n -> (B.concat <$> readResultPure n bs) == readResultPure n (B.concat bs)
