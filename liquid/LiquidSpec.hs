00dd36c912153ea4e8bcbf17a84ab38994c0390f
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-18 18:19:04 +0530

    added flags to enable liquid haskell.

---
+++ liquid/LiquidSpec.hs
@@ -0,0 +1,35 @@
+import Control.Monad
+import Language.Haskell.Liquid.Liquid
+import System.Exit
+import System.Directory
+import System.FilePath
+
+-- | Liquid haskell is run on these files/directors. If the given name
+-- is a directory we recurse on all .hs/.lhs files in that directory.
+toCheck :: [FilePath]
+toCheck = [ "Raaz/Core"
+          ]
+
+main :: IO ()
+main = do allHsFiles toCheck >>= liquid
+
+
+allHsFiles :: [FilePath] -> IO [FilePath]
+allHsFiles = fmap concat . mapM allHsFile
+
+allHsFile :: FilePath -> IO [FilePath]
+allHsFile fp = doesFileExist fp >>= handleFile
+
+  where handleFile cond
+           | cond && checkHs fp = return [fp]
+           | cond               = return []
+           | otherwise          = doesDirectoryExist fp >>= handleDir
+
+        handleDir cond
+           | cond       = recurse
+           | otherwise  = return []
+
+        recurse = listDirectory fp >>= allHsFiles . map (fp</>)
+
+        checkHs fp = ext == ".hs" || ext == ".lhs"
+          where ext = takeExtension fp
