644c546b8158caec909398fb6957760970fb497b
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-05-31 17:17:27 +0530

    more restrictive class RandomStorable instead of Random
    - What a PRG generates is essentially random bytes and hence it makes
      most sense to generate storable elements. The generaic Random looks
      too ambitious and hence untenable as an interface to random data.

--- Raaz/Random.hs
+++ Raaz/Random.hs
@@ -6,10 +6,9 @@ module Raaz.Random
          RandM, RT, liftMT
        , randomByteString
        -- ** Types that can be generated randomly
-       , Random(..)
+       , RandomStorable(..), unsafeFillRandomElements, random
          -- * Low level access to randomness.
        , fillRandomBytes
-       , unsafeStorableRandom
        , reseed
        ) where
 
@@ -34,10 +33,19 @@ import Raaz.Random.ChaCha20PRG
 --
 -- The raaz library gives a relatively high level interface to
 -- randomness. The monad `RandM` captures a batch of actions that
--- generate/use cryptographically secure random bytes. In particular,
--- you can use the functions `random` and `randomByteString` to
--- actually generate random elements.
+-- generate/use cryptographically secure random bytes. The easiest way
+-- to generate a random element is to use the `random` combinator. If
+-- one is interested in a sequence of random bytes, one can use the
+-- `randomBytestring` combinator.
 --
+-- A more low level interface to cryptographic randomness is through
+-- the buffer filling operations `fillRandomBytes` and
+-- `fillRandomElements`. While not as convenient to use as `random`
+-- and `randomByteString`, and in many ways prone to all the problems
+-- with pointer functions, this should be the method of choice for
+-- generating sensitive data as you can make sure that the data does
+-- not end up on the swap device by using a locked memory for the
+-- buffer.
 --
 -- = Running a random action
 --
@@ -166,9 +174,9 @@ instance Memory mem => MonadMemory (RT mem) where
 -- explicitly seed your generator. The insecurely and securely calls
 -- makes sure that your generator is seed before
 -- starting. Furthermore, the generator also reseeds after every few
--- GB of random bytes generates. Generating random data from the
--- system entropy is usually an order of magnitude slower than using a
--- fast stream cipher. Reseeding often can slow your program
+-- GB of random bytes that it generates. Generating random data from
+-- the system entropy is usually an order of magnitude slower than
+-- using a fast stream cipher. Reseeding often can slow your program
 -- considerably without any additional security advantage.
 --
 reseed :: RT mem ()
@@ -179,47 +187,53 @@ fillRandomBytes :: LengthUnit l => l ->  Pointer -> RT mem ()
 fillRandomBytes l = RT . onSubMemory fst . fillRandomBytesMT l
 
 
--- | Types that can be generated at random. It might appear that all
--- storables should be an instance of this class, after all we know
--- the size of the element why not write that many random bytes. In
--- fact, this module provides an `unsafeStorableRandom` which does
--- exactly that. However, we do not give a blanket definition for all
--- storables because for certain refinements of a given type, like for
--- example, Word8's modulo 10, `unsafeStorableRandom` introduces
--- unacceptable skews.
-class Random a where
+-- | Instances of `Storable` which can be randomly generated. It might
+-- appear that all storables can easily be generated randomly should
+-- be instances of this class, after all we know the size of the
+-- element why not write that many random bytes. In fact, this module
+-- provides an `unsafeFillRandomElements` which does that. However, we
+-- do not give a blanket definition for all storables because for
+-- certain refinements of a given type, like for example, Word8's
+-- modulo 10, `unsafeFillRandomElements` introduces unacceptable
+-- skews.
+class Storable a => RandomStorable a where
+  -- | Fill the buffer with so many random elements of type a.
+  fillRandomElements :: Memory mem
+                     => Int       -- ^ number of elements to fill
+                     -> Ptr a     -- ^ The buffer to fill
+                     -> RT mem ()
 
-  random :: Memory mem => RT mem a
 
--- | Generate a random element. The element picked is
--- crypto-graphically pseudo-random.
---
--- This is a helper function that has been exported to simplify the
--- definition of a `Random` instance for `Storable` types. However,
--- there is a reason why we do not give a blanket instance for all
--- instances `Storable` and why this function is unsafe? This function
--- generates a random element of type @a@ by generating @n@ random
--- bytes where @n@ is the size of the elements of @a@. For instances
--- that range the entire @n@ byte space this is fine. However, if the
--- type is actually a refinement of such a type --- consider for
--- example, @`Word8`@ modulo @10@ -- this function generates an
--- unacceptable skew in the distribution. Hence this function is
--- prefixed unsafe.
---
-unsafeStorableRandom :: (Memory mem, Storable a) => RT mem a
-unsafeStorableRandom = RT $ onSubMemory fst retA
-  where retA = liftPointerAction alloc $ getIt . castPtr
-
-        getIt        :: Storable a => Ptr a -> MT RandomState a
-        getIt ptr    = unsafePokeManyRandom 1 ptr >> liftIO (peek ptr)
-        getElement   :: MT RandomState a -> a
+-- | This is a helper function that has been exported to simplify the
+-- definition of a `RandomStorable` instance for `Storable`
+-- types. However, there is a reason why we do not give a blanket
+-- instance for all instances `Storable` and why this function is
+-- unsafe? This function generates a random element of type @a@ by
+-- generating @n@ random bytes where @n@ is the size of the elements
+-- of @a@. For instances that range the entire @n@ byte space this is
+-- fine. However, if the type is actually a refinement of such a type,
+-- (consider a @`Word8`@ modulo @10@ for example) this function
+-- generates an unacceptable skew in the distribution. Hence this
+-- function is prefixed unsafe.
+unsafeFillRandomElements :: (Memory mem, Storable a) => Int -> Ptr a -> RT mem ()
+unsafeFillRandomElements n ptr = fillRandomBytes totalSz $ castPtr ptr
+  where totalSz = fromIntegral n * sizeOf (getElement ptr)
+        getElement :: Ptr a -> a
         getElement _ = undefined
 
-        algn         = alignment $ getElement retA
-        sz           = sizeOf    $ getElement retA
 
-        alloc        = allocaAligned algn sz
-
+-- | Generate a random element from an instance of a RandomStorable
+-- element.
+random :: (RandomStorable a, Memory mem) => RT mem a
+random = RT $ liftPointerAction alloc (getIt . castPtr)
+  where getIt ptr    = unMT $ fillRandomElements 1 ptr >> liftIO (peek ptr)
+        alloc        :: Storable a => (Pointer -> IO a) -> IO a
+        alloc action = allocaAligned algn sz action
+          where getElement   :: (Pointer -> IO b) -> b
+                getElement _ = undefined
+                thisElement  = getElement action
+                algn         = alignment thisElement
+                sz           = sizeOf    thisElement
 
 -- | Generate a random byteString.
 
@@ -230,74 +244,57 @@ randomByteString l = RT $ onSubMemory fst  $ liftPointerAction (create l) $ fill
 
 ------------------------------- Some instances of Random ------------------------
 
-instance Random Word8 where
-  random = unsafeStorableRandom
+instance RandomStorable Word8 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Word16 where
-  random = unsafeStorableRandom
+instance RandomStorable Word16 where
 
-instance Random Word32 where
-  random = unsafeStorableRandom
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Word64 where
-  random = unsafeStorableRandom
+instance RandomStorable Word32 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Word where
-  random = unsafeStorableRandom
+instance RandomStorable Word64 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Int8 where
-  random = unsafeStorableRandom
+instance RandomStorable Word where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Int16 where
-  random = unsafeStorableRandom
+instance RandomStorable Int8 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Int32 where
-  random = unsafeStorableRandom
+instance RandomStorable Int16 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Int64 where
-  random = unsafeStorableRandom
+instance RandomStorable Int32 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random Int where
-  random = unsafeStorableRandom
+instance RandomStorable Int64 where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random KEY where
-  random = unsafeStorableRandom
+instance RandomStorable Int where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random IV where
-  random = unsafeStorableRandom
+instance RandomStorable KEY where
+  fillRandomElements = unsafeFillRandomElements
 
+instance RandomStorable IV where
+  fillRandomElements = unsafeFillRandomElements
 
-instance Random w => Random (LE w) where
-  random = littleEndian <$> random
+instance RandomStorable w => RandomStorable (LE w) where
+  fillRandomElements n = fillRandomElements n . lePtrToPtr
+    where lePtrToPtr :: Ptr (LE w) -> Ptr w
+          lePtrToPtr = castPtr
 
-instance Random w => Random (BE w) where
-  random = bigEndian <$> random
+instance RandomStorable w => RandomStorable (BE w) where
+  fillRandomElements n = fillRandomElements n . bePtrToPtr
+    where bePtrToPtr :: Ptr (BE w) -> Ptr w
+          bePtrToPtr = castPtr
 
-instance (Dimension d, Unbox w, Random w) => Random (Tuple d w) where
-  random = repeatM random
-
--------------------------- Now comes the boring tuples -----------------
-
-instance (Random a, Random b) => Random (a,b) where
-  random = (,) <$> random <*> random
-
-instance (Random a, Random b, Random c) => Random (a,b,c) where
-  random = (,,) <$> random <*> random <*> random
-
-instance (Random a, Random b, Random c, Random d) => Random (a,b,c,d) where
-  random = (,,,) <$> random <*> random <*> random <*> random
-
-instance (Random a, Random b, Random c, Random d, Random e) => Random (a,b,c,d,e) where
-  random = (,,,,) <$> random <*> random <*> random <*> random <*> random
-
--- | The action @unsafePokeManyRandom n ptr@ pokes @n@ random elements
--- at the location starting at ptr.  If the underlying type does not
--- saturate its entire binary size (think of say Word8 modulo 5), the
--- distribution of elements can be rather skewed . Hence the prefix
--- unsafe. This function is exported to simplify the definition
--- `Random` instance. Do not use it unwisely.
-unsafePokeManyRandom :: Storable a => Int -> Ptr a -> MT RandomState ()
-unsafePokeManyRandom n ptr = fillRandomBytesMT totalSz $ castPtr ptr
-  where totalSz = fromIntegral n * sizeOf (getElement ptr)
-        getElement :: Ptr a -> a
-        getElement _ = undefined
+instance (Dimension d, Unbox w, RandomStorable w) => RandomStorable (Tuple d w) where
+  fillRandomElements n ptr = fillRandomElements (n * sz) $ tupPtrToPtr ptr
+    where getTuple    :: Dimension d => Ptr (Tuple d w) -> Tuple d w
+          getTuple _  = undefined
+          tupPtrToPtr ::  Ptr (Tuple d w) -> Ptr w
+          tupPtrToPtr = castPtr
+          sz         = dimension $ getTuple ptr
