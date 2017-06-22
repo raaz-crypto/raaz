6e6586c093fa8da7a179d440a80838c0d4d849e9
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-06-22 13:53:33 +0530

    rewrite to ensure LH finds termination.

--- Raaz/Core/Types/Tuple.hs
+++ Raaz/Core/Types/Tuple.hs
@@ -137,11 +137,10 @@ instance (V.Unbox a, EndianStore a, Dimension dim)
 
 -- | Construct a tuple by repeating a monadic action.
 repeatM :: (Functor m, Monad m, V.Unbox a, Dimension dim) => m a -> m (Tuple dim a)
-repeatM action = result
-  where result = Tuple <$> V.replicateM sz action
-        sz     = dimension $ getTup result
-        getTup :: (Monad m, Dimension n)=> m (Tuple n a) -> Tuple n a
-        getTup _ = undefined
+repeatM = mkTupM undefined
+  where mkTupM :: (V.Unbox a, Monad m, Dimension dim) => Tuple dim a -> m a -> m (Tuple dim a)
+        mkTupM uTup action = Tuple <$> V.replicateM (dimension uTup) action
+
 
 -- | Construct a tuple out of the list. This function is unsafe and
 -- will result in run time error if the list is not of the correct
@@ -160,18 +159,21 @@ unsafeFromList xs
 
 -- | Computes the initial fragment of a tuple. No length needs to be given
 -- as it is infered from the types.
+{-@ lazy initial @-}
 initial ::  (V.Unbox a, Dimension dim0)
          => Tuple dim1 a
          -> Tuple dim0 a
-initial tup = tup0
-  where tup0 = Tuple $ V.take (dimension tup0) $ unTuple tup
+initial = mkTuple undefined
+  where mkTuple :: (V.Unbox a, Dimension dim0) => Tuple dim0 a -> Tuple dim1 a  -> Tuple dim0 a
+        mkTuple uTup tup = Tuple $ V.take (dimension uTup) $ unTuple tup
 
 -- TODO: Put a constraint that dim0 <= dim1
 
 -- | The @diagonal a@ gives a tuple, all of whose entries is @a@.
 diagonal :: (V.Unbox a, Dimension dim) => a -> Tuple dim a
-diagonal a = tup
-  where tup = Tuple $ V.replicate (dimension tup) a
+diagonal = mkTup undefined
+  where mkTup :: (V.Unbox a, Dimension dim) => Tuple dim a -> a -> Tuple dim a
+        mkTup uTup a = Tuple $ V.replicate (dimension uTup) a
 
 
 -- | A zipwith function for tuples
