db77920247f50dfe2ef839dce638433a6621bdb6
Tamar Christina <tamar@zhox.com>
2017-06-24 12:41:03 +0100

    Windows: Add memory locking with VirtualLock/VirtualUnlock.

--- Raaz/Core/Types/Pointer.hs
+++ Raaz/Core/Types/Pointer.hs
@@ -322,23 +322,37 @@ allocaSecureAligned :: LengthUnit l
                     -> (Pointer -> IO a)
                     -> IO a
 
+#if defined(HAVE_MLOCK) || defined(PLATFORM_WINDOWS)
+
 #ifdef HAVE_MLOCK
 
 foreign import ccall unsafe "sys/mman.h mlock"
   c_mlock :: Pointer -> BYTES Int -> IO Int
 
-
 foreign import ccall unsafe "sys/mman.h munlock"
   c_munlock :: Pointer -> BYTES Int -> IO ()
+
+#else
+
+foreign import ccall unsafe "Windows.h VirtualLock"
+  c_mlock :: Pointer -> BYTES Int -> IO Int
+
+foreign import ccall unsafe "Windows.h VirtualUnlock"
+  c_munlock :: Pointer -> BYTES Int -> IO ()
+
+#endif
 
 allocaSecureAligned a l action = allocaAligned a l actualAction
   where sz = inBytes l
         actualAction cptr = let
           lockIt    = do c <- c_mlock cptr sz
+#ifdef HAVE_MLOCK
                          when (c /= 0) $ fail "secure memory: unable to lock memory"
+#else
+                         when (c == 0) $ fail "secure memory: unable to lock memory"
+#endif
           releaseIt =  memset cptr 0 l >>  c_munlock cptr sz
           in bracket_ lockIt releaseIt $ action cptr
-
 #else
 allocaSecureAligned _ _ = fail "memory locking not supported on this platform"
 
--- cbits/raaz/core/memory.c
+++ cbits/raaz/core/memory.c
@@ -1,4 +1,7 @@
 #include <raaz/core/memory.h>
+#ifdef PLATFORM_WINDOWS
+#  include <windows.h>
+#endif
 
 /* Wipes the entire memory with 0. */
 void wipememory ( volatile void* mem, /* volatile is used to keep the
@@ -21,10 +24,16 @@ int memorylock(void* ptr, size_t size){
 #ifdef RAAZ_HAVE_mlock
   return mlock(ptr,size);
 #endif
+#ifdef PLATFORM_WINDOWS
+  return VirtualLock(ptr, size);
+#endif
 }
 
 void memoryunlock(void* ptr, size_t size){
 #ifdef RAAZ_HAVE_mlock
   munlock(ptr,size);
+#endif
+#ifdef PLATFORM_WINDOWS
+  VirtualUnlock(ptr, size);
 #endif
 }
