6c88d24cfc4913bfcc64c3154225a171cec78201
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-06 18:15:10 +0530

    memory.c cleaned up and simplified.

--- cbits/raaz/core/memory.c
+++ cbits/raaz/core/memory.c
@@ -1,49 +1,34 @@
-#include <raaz/core/memory.h>
-#ifdef PLATFORM_WINDOWS
-#  include <windows.h>
-#endif
+# ifdef PLATFORM_WINDOWS
+# include <windows.h>
 
-#ifdef PLATFORM_WINDOWS
-typedef BOOL WINAPI (*VirtualFunction)(LPVOID, SIZE_T);
-#endif
+   typedef BOOL WINAPI (*VirtualFunction)(LPVOID, SIZE_T);
 
-/* Wipes the entire memory with 0. */
-void wipememory ( volatile void* mem, /* volatile is used to keep the
-                                      ** compiler from optimising
-                                      ** stuff.
-                                      ** BIG QUESTION: Is this
-                                      ** sufficient?
-				      */
-		  size_t size
-                )
-{
-    /* WARNING: Potentially dangerous code. Please audit all changes
-    ** carefully.
-    */
-    memset((void *)mem, 0, size);
-}
+#else /* we assume posix system */
+
+#  include <sys/mman.h>
+
+#endif
 
 /* Locks and Unlocks Memory */
 int memorylock(void* ptr, size_t size){
-#ifdef RAAZ_HAVE_mlock
-  return mlock(ptr,size);
-#endif
 #ifdef PLATFORM_WINDOWS
   VirtualFunction func =
      (VirtualFunction)GetProcAddress(GetModuleHandle(TEXT("kernel32")),
 				     "VirtualLock");
   return !func(ptr, size);
+#else
+    return mlock(ptr,size);
 #endif
+
 }
 
 void memoryunlock(void* ptr, size_t size){
-#ifdef RAAZ_HAVE_mlock
-  munlock(ptr,size);
-#endif
 #ifdef PLATFORM_WINDOWS
   VirtualFunction func =
      (VirtualFunction)GetProcAddress(GetModuleHandle (TEXT("kernel32")),
 				     "VirtualUnlock");
   func(ptr, size);
+#else  /* posix */
+    munlock(ptr,size);
 #endif
 }
