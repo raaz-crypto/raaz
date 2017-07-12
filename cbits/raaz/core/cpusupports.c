572f4fc7330b32cd0a0477c04219a05b88ae92e4
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-07-12 18:13:08 +0530

    gcc: some gcc version checks.

--- cbits/raaz/core/cpusupports.c
+++ cbits/raaz/core/cpusupports.c
@@ -4,35 +4,39 @@ This module exposes runtime checks for cpu flags. Works only with GCC.
 
  */
 
-# ifdef __clang__
-# define SUPPORTS(x) { return 0; }
-# elif defined (__GNUC__)
-# define SUPPORTS(x) { __builtin_cpu_init(); return __builtin_cpu_supports((x)); }
+
+# define GCC_VERSION (__GNUC__ * 10 + __GNUC_MINOR__)
+
+/* Check an x86_64 feature */
+
+# ifdef ARCH_X86_64
+#     define X86_64_SUPPORTS(x) { __builtin_cpu_init(); return __builtin_cpu_supports((x)); }
 # else
-# define SUPPORTS(x) { return 0; }
+#     define X86_64_SUPPORTS(x) { return 0; }
 # endif
 
-
+# if GCC_VERSION >= 48 && !defined(__clang__)
 # pragma GCC push_options
 # pragma GCC optimize ("O0")
 
-# ifdef ARCH_X86_64
-int raaz_supports_sse(){ SUPPORTS("sse");}
-int raaz_supports_sse2(){ SUPPORTS("sse2");}
-int raaz_supports_sse3(){ SUPPORTS("sse3");}
-int raaz_supports_sse4_1(){ SUPPORTS("sse4.1");}
-int raaz_supports_sse4_2(){ SUPPORTS("sse4.2");}
-int raaz_supports_avx()   { SUPPORTS("avx");}
-int raaz_supports_avx2(){ SUPPORTS("avx2");}
+int raaz_supports_sse()   { X86_64_SUPPORTS("sse");     }
+int raaz_supports_sse2()  { X86_64_SUPPORTS("sse2");    }
+int raaz_supports_sse3()  { X86_64_SUPPORTS("sse3");    }
+int raaz_supports_sse4_1(){ X86_64_SUPPORTS("sse4.1");  }
+int raaz_supports_sse4_2(){ X86_64_SUPPORTS("sse4.2");  }
+int raaz_supports_avx()   { X86_64_SUPPORTS("avx");     }
+int raaz_supports_avx2()  { X86_64_SUPPORTS("avx2");    }
+
+# pragma GCC pop_options
+
 # else
-int raaz_supports_sse(){ return 0;}
-int raaz_supports_sse2(){ return 0;}
-int raaz_supports_sse3(){ return 0;}
+
+int raaz_supports_sse()   { return 0;}
+int raaz_supports_sse2()  { return 0;}
+int raaz_supports_sse3()  { return 0;}
 int raaz_supports_sse4_1(){ return 0;}
 int raaz_supports_sse4_2(){ return 0;}
 int raaz_supports_avx()   { return 0;}
-int raaz_supports_avx2(){ return 0;}
+int raaz_supports_avx2()  { return 0;}
 
 #endif
-
-# pragma GCC pop_options
