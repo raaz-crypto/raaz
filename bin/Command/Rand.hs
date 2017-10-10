5e6525d57a6e5687c80a5812c407d7110f83d012
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-10-10 19:46:51 +0530

    added an entropy command that parallels rand
    - useful to test the system entropy quality.

--- bin/Command/Rand.hs
+++ bin/Command/Rand.hs
@@ -12,6 +12,7 @@ import Control.Monad.IO.Class(liftIO)
 import Data.Monoid
 import Options.Applicative
 import Raaz
+import Raaz.Random.Internal
 import System.IO
 
 -- So much bytes generated in one go before writing to stdout.
@@ -20,12 +21,6 @@ bufSize :: BYTES Int
 bufSize = 32 * 1024
 
 
-opts :: Parser (IO ())
-opts =   nRandomBytes . toEnum <$> argument auto (metavar "NUMBER_OF_BYTES")
-     <|> pure infinteBytes
-  where nRandomBytes n = withBuffer $ insecurely  . genBytes n
-        infinteBytes = withBuffer $ insecurely  . genInfiniteBytes
-        withBuffer = allocaBuffer bufSize
 
 
 
@@ -33,29 +28,58 @@ opts =   nRandomBytes . toEnum <$> argument auto (metavar "NUMBER_OF_BYTES")
 rand :: Parser (IO ())
 
 #if MIN_VERSION_optparse_applicative(0,13,0)
-rand = subparser $ commandGroup "Randomness" <> randCmd <> metavar "rand"
+rand = subparser $ commandGroup "Randomness"
 #else
-rand = subparser $ randCmd <> metavar "rand"
+rand = subparser $ mempty
 #endif
+  <> randCmd    <> metavar "rand"
+  <> entropyCmd <> metavar "entropy"
+  where
 
-  where randCmd = command "rand" $ info (helper <*> opts) $ fullDesc
+
+randCmd :: Mod CommandFields (IO ())
+randCmd = command "rand" $ info (helper <*> randOpts) $ fullDesc
           <> header "raaz rand - Cryptographically secure pseudo random bytes."
           <> progDesc "Generate cryptographically secure pseudo random bytes onto the stdout."
+  where randOpts = opts insecurely emitRand
+
+entropyCmd :: Mod CommandFields (IO ())
+entropyCmd = command "entropy" $ info (helper <*> entropyOpts) $ fullDesc
+          <> header "raaz entropy - System entropy."
+          <> progDesc "Emit data from the system entropy pool."
+  where entropyOpts = opts id emitEntropy
+
+opts :: Monad m
+     => (m () -> IO ())                -- ^ The runner
+     -> (BYTES Int -> Pointer -> m ()) -- ^ The filler
+     -> Parser (IO ())
+opts runner filler = nRandomBytes . toEnum <$> argument auto (metavar "NUMBER_OF_BYTES")
+                     <|> pure infinteBytes
+  where nRandomBytes n = withBuffer $ runner . genBytes filler n
+        infinteBytes   = withBuffer $ runner . genInfiniteBytes filler
+        withBuffer     = allocaBuffer bufSize
 
 
 
 
-genInfiniteBytes :: Pointer -> RandM ()
-genInfiniteBytes ptr = goForEver
-  where goForEver = emitRand bufSize ptr >> goForEver
+genInfiniteBytes :: Monad m
+                 => (BYTES Int -> Pointer -> m ()) -- ^ The filler function
+                 -> Pointer  -- ^ the buffer to fill
+                 -> m ()
+genInfiniteBytes filler ptr = goForEver
+  where goForEver = filler bufSize ptr >> goForEver
 
 
 -- Generate so many bytes.
 
-genBytes :: BYTES Int -> Pointer-> RandM ()
-genBytes n ptr = go n
-  where go m | m >= bufSize = do emitRand bufSize ptr; go (m - bufSize)
-             | otherwise    =    emitRand m ptr
+genBytes :: Monad m
+         => (BYTES Int -> Pointer -> m ())
+         -> BYTES Int
+         -> Pointer
+         -> m ()
+genBytes filler n ptr = go n
+  where go m | m >= bufSize = do filler bufSize ptr; go (m - bufSize)
+             | otherwise    =    filler m ptr
 
 
 -- Emit so may random bytes.
@@ -64,3 +88,8 @@ emitRand :: BYTES Int -> Pointer-> RandM ()
 emitRand m ptr = do
   fillRandomBytes m ptr
   liftIO $ hPutBuf stdout ptr $ fromIntegral m
+
+emitEntropy :: BYTES Int -> Pointer -> IO ()
+emitEntropy m ptr = do
+  fillSystemEntropy m ptr
+  hPutBuf stdout ptr $ fromIntegral m
