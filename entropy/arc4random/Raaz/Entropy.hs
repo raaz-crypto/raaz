083b50f009f29c34c672b7084d56f5128f4e892e
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-10-10 16:40:24 +0530

    update the docs in arc4random entropy.

--- entropy/arc4random/Raaz/Entropy.hs
+++ entropy/arc4random/Raaz/Entropy.hs
@@ -7,16 +7,16 @@ import Control.Monad.IO.Class(MonadIO, liftIO)
 import Raaz.Core.Types
 
 
--- | The getrandom system call.
+-- | The arc4random function.
 foreign import ccall unsafe
   "arc4random_buf"
   c_arc4random :: Pointer      -- Message
                -> BYTES Int    -- number of bytes
                -> IO (BYTES Int)
 
--- | Get random bytes from using the @getrandom@ system call on
--- linux. This is only used to seed the PRG and not intended for call
--- by others.
+-- | Get random bytes from using the @arc4random@ on OpenBSD/NetBSD
+-- This is only used to seed the PRG and not intended for call by
+-- others.
 getEntropy :: (MonadIO m, LengthUnit l) => l -> Pointer -> m (BYTES Int)
 getEntropy l ptr = liftIO $ c_arc4random ptr lenBytes >> return lenBytes
   where lenBytes = inBytes l
