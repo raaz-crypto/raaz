f4d5f3d3707deb812756d133f31aa63d414713a2
Piyush P Kurur <ppk@cse.iitk.ac.in>
2017-10-11 10:21:31 +0530

    raaz commad updated to print library information.

--- bin/Command/Info.hs
+++ bin/Command/Info.hs
@@ -1,32 +1,51 @@
 {-# LANGUAGE CPP #-}
-module Command.CpuInfo where
+module Command.Info where
 
 import Data.Monoid
+import Data.Version (showVersion)
 import Options.Applicative
-
+import Raaz
 import Raaz.Core.CpuSupports as CpuSupports
+import Raaz.Random.Internal
 
-display :: (Bool, String) -> String
-display (True, cap) = unwords ["+", cap]
-display (_,cap)     = unwords ["-", cap]
 
-cpuInfo :: Parser (IO ())
-
+information :: Parser (IO ())
 
 #if MIN_VERSION_optparse_applicative(0,13,0)
-cpuInfo = subparser $ commandGroup "CPU capabilities" <> cpuInfoCmd <> metavar "cpuinfo"
+information = subparser $ commandGroup "Information" <> infoCmd <> metavar "info"
 #else
-cpuInfo = subparser $ cpuInfoCmd <> metavar "cpuinfo"
+information = subparser $ infoCmd <> metavar "info"
 #endif
-  where cpuInfoCmd = command "cpuinfo" $ info (helper <*> opts) $ fullDesc
-                     <> header "raaz cpuinfo - CPU capabilities detected at runtime."
-                     <> progDesc "Shows the capabilities understood/detected by raaz at runtime. Recommended implementations depend on this."
+  where infoCmd = command "info" $ info (helper <*> opts) $ fullDesc
+                     <> header "raaz info - Print the library information"
+                     <> progDesc "Prints information about raaz library."
 
-        opts = pure $ mapM_ (putStrLn . display) [ (CpuSupports.sse, "sse")
-                                                 , (CpuSupports.sse2, "sse2")
-                                                 , (CpuSupports.sse3, "sse3")
-                                                 , (CpuSupports.sse4_1, "sse4.1")
-                                                 , (CpuSupports.sse4_2, "sse4.2")
-                                                 , (CpuSupports.avx,    "avx")
-                                                 , (CpuSupports.avx2,   "avx2")
-                                                 ]
+        opts = pure $ sequence_ [ field "Library Version" $ showVersion version
+                                , field "Entropy" entropySource
+                                , field "CSPRG"  csPRG
+                                , cpuCapabilities
+
+                                ]
+
+
+
+field   :: String -> String -> IO ()
+field title value = putStrLn $ title ++ ": " ++ value
+section :: String -> [String] -> IO ()
+section title lines = do
+  putStrLn $ title ++ ":"
+  mapM_ indent lines
+  where indent = putStrLn . (++) "    "
+
+cpuCapabilities :: IO ()
+cpuCapabilities = section "CPU capabilities"
+                  $ map display $ [ (CpuSupports.sse, "sse")
+                                  , (CpuSupports.sse2, "sse2")
+                                  , (CpuSupports.sse3, "sse3")
+                                  , (CpuSupports.sse4_1, "sse4.1")
+                                  , (CpuSupports.sse4_2, "sse4.2")
+                                  , (CpuSupports.avx,    "avx")
+                                  , (CpuSupports.avx2,   "avx2")
+                                  ]
+                  where display (True, cap) = unwords ["+", cap]
+                        display (_,cap)     = unwords ["-", cap]
