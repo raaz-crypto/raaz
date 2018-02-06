863c1e7e981fb0f13ac534c9f2cae9aa8e4b8190
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-02-06 12:45:34 +0530

    made stack-freeze script more versatile

--- scripts/stack-freeze.sh
+++ scripts/stack-freeze.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 STACKAGE_URL=https://www.stackage.org/"$1"/cabal.config
+TARGET=cabal.project.freeze
+if [ -n "$2" ]
+then
+   TARGET="$2"
+fi
+
 rm -f cabal.config
 wget "$STACKAGE_URL" -c
-sed -n /raaz/!p < cabal.config > cabal.project.freeze
+sed -n /raaz/!p < cabal.config > "$TARGET"
