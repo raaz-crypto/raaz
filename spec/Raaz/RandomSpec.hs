e3842d9f31e092ea729919e5013df0926b7a990a
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-06-01 16:43:00 +0530

    enable the trivial distinctness test of prg.

--- spec/Raaz/RandomSpec.hs
+++ spec/Raaz/RandomSpec.hs
@@ -1,13 +1,11 @@
 module Raaz.RandomSpec where
 
 import Common
--- import Raaz.Random
+import Raaz.Random
 
 spec :: Spec
-spec = it "randomness" pending
-
--- spec = it "system prg should return different words on distinct calls"
---        $ compareWords `shouldReturn` False
---   where r64                 :: RandM Word64
---         r64                 = random
---         compareWords        = insecurely $ (==) <$> r64 <*> r64
+spec = it "system prg should return different words on distinct calls"
+       $ compareWords `shouldReturn` False
+  where r64                 :: RandM Word64
+        r64                 = random
+        compareWords        = insecurely $ (==) <$> r64 <*> r64
