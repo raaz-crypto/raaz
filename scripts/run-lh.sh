e34f5e6ee37c921faeef416d930b8ffc6edf3ecd
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-07-20 17:17:11 +0530

    simplified the liquid haskell driver script.

--- scripts/run-lh.sh
+++ scripts/run-lh.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
+
 LIQUID="$HOME/liquidhaskell/.cabal-sandbox/bin/liquid"
 $LIQUID --version
+
 echo "-------------------------------------------------"
-pwd
-echo "-------------------------------------------------"
-cd ./raaz-core
-pwd
-echo "-------------------------------------------------"
-find . -name '*.hs' -print0 | xargs -n 1 -0 -I{} sh -c "echo ---- {}; $LIQUID {}"
-cd ..
-pwd
-echo "-------------------------------------------------"
-cd raaz-implementation
-pwd
-echo "-------------------------------------------------"
-find . -name '*.hs' -print0 | xargs -n 1 -0 -I{} sh -c "echo ---- {}; $LIQUID {}"
+
+
+"$LIQUID" `find raaz-core -name "*.hs" | grep -v Entropy`
+"$LIQUID"`find raaz-implementation -name "*.hs" | grep -v Entropy`
