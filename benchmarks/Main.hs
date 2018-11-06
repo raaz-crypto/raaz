e1f949372853f141e2e37892cfe602eb91885e67
Piyush P Kurur <ppk@cse.iitk.ac.in>
2018-11-06 13:40:04 +0530

    benchmark against handwritten ones.

--- benchmarks/Main.hs
+++ benchmarks/Main.hs
@@ -10,19 +10,33 @@ import Control.Monad
 import Text.PrettyPrint
 
 import           Benchmark.Types
-import qualified Benchmark.Blake2b.CPortable  as Blake2bCP
-import qualified Benchmark.Blake2s.CPortable  as Blake2sCP
-import qualified Benchmark.ChaCha20.CPortable as ChaCha20CP
-import qualified Benchmark.Sha256.CPortable   as Sha256CP
-import qualified Benchmark.Sha512.CPortable   as Sha512CP
+import qualified Benchmark.Blake2b.CPortable     as Blake2bCP
+import qualified Benchmark.Blake2b.CHandWritten  as Blake2bCHW
+
+import qualified Benchmark.Blake2s.CHandWritten  as Blake2sCHW
+
+import qualified Benchmark.ChaCha20.CPortable    as ChaCha20CP
+
+import qualified Benchmark.Sha256.CPortable      as Sha256CP
+import qualified Benchmark.Sha256.CHandWritten   as Sha256CHW
+
+import qualified Benchmark.Sha512.CPortable      as Sha512CP
+import qualified Benchmark.Sha512.CHandWritten   as Sha512CHW
 
 
 main :: IO ()
 main = mapM_ printBench [ Blake2bCP.bench
-                        , Blake2sCP.bench
+                        , Blake2bCHW.bench
+
+                        , Blake2sCHW.bench
+
                         , ChaCha20CP.bench
+
                         , Sha256CP.bench
+                        , Sha256CHW.bench
+
                         , Sha512CP.bench
+                        , Sha512CHW.bench
                         ]
 
   where printBench = runRaazBench >=> pure . render >=> putStrLn
