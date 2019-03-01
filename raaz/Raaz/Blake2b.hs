14229fe66cf68c283d0ef4fa60e74d2a25390eaa
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-03-01 17:22:02 +0530

    expose interface for MACing with blake2b and blake2s.

--- raaz/Raaz/Blake2b.hs
+++ raaz/Raaz/Blake2b.hs
@@ -5,11 +5,17 @@ module Raaz.Blake2b
        , digest
        , digestFile
        , digestSource
+       , auth, verify
+       , authFile, verifyFile
+       , authSource, verifySource
        ) where
 
 import qualified Blake2b.Interface as U
+import qualified Blake2b.Mac.Interface as A
 import           Raaz.Core
 import           Raaz.Primitive.Blake2.Internal ( Blake2b )
+import           Raaz.Primitive.Keyed.Internal  (Keyed)
+
 
 -- | Compute the digest of a pure byte source like, `B.ByteString`.
 digest :: PureByteSource src
@@ -29,3 +35,47 @@ digestSource :: ByteSource src
              -> IO Blake2b
 digestSource = U.digestSource
 
+-- | Use the blake2b hashing algorithm as a message authenticator
+-- using the keyed hashing algorithm for blake2b.
+auth :: PureByteSource src
+     => Key (Keyed Blake2b)
+     -> src  -- ^ Message
+     -> Keyed Blake2b
+auth  = A.auth
+
+-- | Verify a message using the authentication tag.
+verify :: PureByteSource src
+       => Key (Keyed Blake2b) -- ^ The secret key used.
+       -> Keyed Blake2b       -- ^ The authentication tag.
+       -> src                 -- ^ Message to authenticate.
+       -> Bool
+verify = A.verify
+  -- The equality checking by design timing safe so do not worry.
+
+-- | Compute the auth of file.
+authFile :: Key (Keyed Blake2b)
+         -> FilePath  -- ^ File to be authed
+         -> IO (Keyed Blake2b)
+authFile = A.authFile
+
+-- | Verify
+verifyFile :: Key (Keyed Blake2b)
+           -> Keyed Blake2b
+           -> FilePath
+           -> IO Bool
+verifyFile = A.verifyFile
+
+-- | Compute the auth of an arbitrary byte source.
+authSource :: ByteSource src
+           => Key (Keyed Blake2b)
+           -> src
+           -> IO (Keyed Blake2b)
+
+authSource = A.authSource
+
+verifySource :: ByteSource src
+             => Key (Keyed Blake2b)
+             -> Keyed Blake2b
+             -> src
+             -> IO Bool
+verifySource = A.verifySource
--- raaz/Raaz/Blake2s.hs
+++ raaz/Raaz/Blake2s.hs
@@ -5,12 +5,17 @@ module Raaz.Blake2s
        , digest
        , digestFile
        , digestSource
+       , auth, verify
+       , authFile, verifyFile
+       , authSource, verifySource
+
        ) where
 
 import qualified Blake2s.Interface as U
+import qualified Blake2s.Mac.Interface as A
 import           Raaz.Core
 import           Raaz.Primitive.Blake2.Internal ( Blake2s )
-
+import           Raaz.Primitive.Keyed.Internal  (Keyed)
 
 -- | Compute the digest of a pure byte source like, `B.ByteString`.
 digest :: PureByteSource src
@@ -30,3 +35,47 @@ digestSource :: ByteSource src
              -> IO Blake2s
 digestSource = U.digestSource
 
+-- | Use the blake2b hashing algorithm as a message authenticator
+-- using the keyed hashing algorithm for blake2b.
+auth :: PureByteSource src
+     => Key (Keyed Blake2s)
+     -> src  -- ^ Message
+     -> Keyed Blake2s
+auth  = A.auth
+
+-- | Verify a message using the authentication tag.
+verify :: PureByteSource src
+       => Key (Keyed Blake2s) -- ^ The secret key used.
+       -> Keyed Blake2s       -- ^ The authentication tag.
+       -> src                 -- ^ Message to authenticate.
+       -> Bool
+verify = A.verify
+  -- The equality checking by design timing safe so do not worry.
+
+-- | Compute the auth of file.
+authFile :: Key (Keyed Blake2s)
+         -> FilePath  -- ^ File to be authed
+         -> IO (Keyed Blake2s)
+authFile = A.authFile
+
+-- | Verify
+verifyFile :: Key (Keyed Blake2s)
+           -> Keyed Blake2s
+           -> FilePath
+           -> IO Bool
+verifyFile = A.verifyFile
+
+-- | Compute the auth of an arbitrary byte source.
+authSource :: ByteSource src
+           => Key (Keyed Blake2s)
+           -> src
+           -> IO (Keyed Blake2s)
+
+authSource = A.authSource
+
+verifySource :: ByteSource src
+             => Key (Keyed Blake2s)
+             -> Keyed Blake2s
+             -> src
+             -> IO Bool
+verifySource = A.verifySource
