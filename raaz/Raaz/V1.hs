5330036fd07a070a1ae5e48222ce1a6c9056a460
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-03-08 14:10:03 +0530

    considerably simplified Raaz.V1 interface.

--- raaz/Raaz/V1.hs
+++ raaz/Raaz/V1.hs
@@ -1,45 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 -- | Version 1 of the interface.
 module Raaz.V1 ( Digest
-               , digest, digestFile, digestSource
+               -- For digests and message authentication.
+               , module Raaz.Blake2b
                ) where
 
-import           Data.String      ( IsString (..) )
-import           Foreign.Storable ( Storable )
+import           Raaz.Blake2b
 
-import           Raaz.Core
-import qualified Raaz.Blake2b    as B2b
-
-
-newtype Digest = Digest B2b.Blake2b deriving ( Equality
-                                             , Eq
-                                             , Storable
-                                             , EndianStore
-                                             , Encodable
-                                             )
-
-instance Show Digest where
-  show (Digest dst) = show dst
-
-instance IsString Digest where
-  fromString = Digest . fromString
-
-
-
--- | Compute the digest of a pure byte source like, `B.ByteString`.
-digest :: PureByteSource src
-       => src              -- ^ Message
-       -> Digest
-digest = Digest . B2b.digest
-
--- | Compute the digest of file.
-digestFile :: FilePath     -- ^ File to be digested
-           -> IO Digest
-digestFile = fmap Digest . B2b.digestFile
-
--- | Compute the digest of an arbitrary byte source.
-digestSource :: ByteSource src
-             => src        -- ^ The source whose digest needs to be
-                           -- computed.
-             -> IO Digest
-digestSource = fmap Digest . B2b.digestSource
+type Digest = Blake2b
