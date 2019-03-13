6e1a9a7cc9954dde26971f78cc1b456007a8625e
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-03-13 10:30:03 +0530

    added a test-auth package for testing authenticators.

--- tests/Raaz/Mac/Poly1305Spec.hs
+++ tests/Raaz/Mac/Poly1305Spec.hs
@@ -7,21 +7,10 @@
 module Raaz.Mac.Poly1305Spec where
 
 import           Tests.Core
-import           Poly1305.Interface
+import           Poly1305.Auth
 import           Raaz.Random
 import qualified Data.ByteString as BS
 import           Raaz.Primitive.Poly1305.Internal (R)
-
-macsTo :: ByteString -> Poly1305 -> Key Poly1305 -> Spec
-macsTo inp expected key =  it msg $ result `shouldBe` expected
-  where result = auth key inp
-        msg  = unwords [ "with key"
-                       , shortened $ show key
-                       , "macs"
-                       , shortened $ show inp
-                       , "to"
-                       , shortened $ show expected
-                       ]
 
 randomClamping :: Spec
 randomClamping = it "randomly generated R values should be clamped"
@@ -55,4 +44,4 @@ spec = do
   with ( "85:d6:be:78:57:55:6d:33:7f:44:52:fe:42:d5:06:a8"
        , "01:03:80:8a:fb:0d:b2:fd:4a:bf:f6:af:41:49:f5:1b"
        ) $ ( "Cryptographic Forum Research Group" :: ByteString)
-    `macsTo` ("a8:06:1d:c1:30:51:36:c6:c2:2b:8b:af:0c:01:27:a9" :: Poly1305)
+    `authsTo` ("a8:06:1d:c1:30:51:36:c6:c2:2b:8b:af:0c:01:27:a9" :: Poly1305)
