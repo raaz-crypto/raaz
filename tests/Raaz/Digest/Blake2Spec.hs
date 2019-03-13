a6b9024ba1878e37f0bd76f6273a7baa7fb955e1
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-03-13 10:33:14 +0530

    add tests for blake2b keyed hashing.

--- tests/Raaz/Digest/Blake2Spec.hs
+++ tests/Raaz/Digest/Blake2Spec.hs
@@ -9,20 +9,34 @@ import           Prelude hiding (replicate)
 import           Tests.Core
 import           Raaz.Primitive.Blake2.Internal(Blake2b, Blake2s)
 import qualified Blake2b.Digest as B2b
+import qualified Blake2b.Auth   as B2b
 import qualified Blake2s.Digest as B2s
+import qualified Blake2s.Auth   as B2s
 
 spec2b :: Spec
 spec2b = describe "blake2b" $ do
   basicEndianSpecs (undefined :: Blake2b)
-
   ------------- Unit tests -------------------------
-  "" `digestsTo`
-    "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce"
-  "abc" `digestsTo`
-    "ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923"
+  let digestsTo = B2b.digestsTo
+    in do "" `digestsTo`
+            "786a02f742015903c6c6fd852552d272912f4740e15847618a86e217f71f5419d25e1031afee585313896444934eb04b903a685b1448b755d56f701afe9be2ce"
+          "abc" `digestsTo`
+            "ba80a53f981c4d0d6a2797b69f12f6e94c212f14685ac4b74b12bb6fdbffa2d17d87c5392aab792dc252d5de4533cc9518d38aa8dbf1925ab92386edd4009923"
 
-  where digestsTo = B2b.digestsTo
+  let
+    keyString = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f"
+    key              = fromString keyString
+    keyMesg          = "with key " ++ shortened keyString
+    authsTo bs prim  = B2b.authsTo bs prim key
 
+    in describe keyMesg $ do
+    "" `authsTo` "10ebb67700b1868efb4417987acf4690ae9d972fb7a590c2f02871799aaa4786b5e996e8f0f4eb981fc214b005f42d2ff4233499391653df7aefcbc13fc51568"
+
+    "\0" `authsTo` "961f6dd1e4dd30f63901690c512e78e4b45e4742ed197c3c5e45c549fd25f2e4187b0bc9fe30492b16b0d0bc4ef9b0f34c7003fac09a5ef1532e69430234cebd"
+
+    "\0\1" `authsTo` "da2cfbe2d8409a0f38026113884f84b50156371ae304c4430173d08a99d9fb1b983164a3770706d537f49e0c916d9f32b95cc37a95b99d857436f0232c88a965"
+
+    fromBase16 "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9fa0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebfc0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedfe0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfe" `authsTo` "142709d62e28fcccd0af97fad0f8465b971e82201dc51070faa0372aa43e92484be1c1e73ba10906d5d1853db6a4106e0a7bf9800d373d6dee2d46d62ef2a461"
 
 spec2s :: Spec
 spec2s = describe "blake2s" $ do
