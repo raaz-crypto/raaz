c40197165def180e243782fb3e79b660fe6e46d6
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-03-25 18:26:50 +0530

    comparison between two hash implementation.

---
+++ tests/message-digest/Tests/Digest/Compare.hs
@@ -0,0 +1,19 @@
+-- This module compares two different implementations of the same
+-- digest.
+{-# LANGUAGE ScopedTypeVariables #-}
+module Tests.Digest.Compare (specCompare) where
+
+
+
+import Data.ByteString (ByteString)
+import Implementation
+import Interface
+import Alternate
+import AlternateInterface
+import Tests.Core
+
+specCompare :: Spec
+specCompare = describe title $ do
+  prop "should hash same strings to same hashes" $ \ (x :: ByteString) ->
+    (show $ Interface.digest x) `shouldBe` (show $ AlternateInterface.digest x)
+  where title = Implementation.name ++ " vs " ++ Alternate.name
