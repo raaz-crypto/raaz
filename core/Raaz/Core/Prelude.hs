fed8abbc9bb26c9f669e1e46dbcd20af2d1acc05
Piyush P Kurur <ppk@cse.iitk.ac.in>
2019-04-06 18:08:26 +0530

    added a custom prelude to cleanup CPP mess.

---
+++ core/Raaz/Core/Prelude.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE CPP #-}
+module Raaz.Core.Prelude ( module X
+                         , module Prelude
+#if !MIN_VERSION_base(4,11,0)
+                         , (<>)
+#endif
+                         ) where
+
+import Control.Applicative          as X
+import Control.Monad                as X
+import Data.Bits                    as X
+import Data.ByteString.Char8           ()
+import Data.ByteString.Lazy.Char8      ()
+import Data.Maybe                   as X
+import Data.Proxy                   as X
+#if !MIN_VERSION_base(4,11,0)
+import Data.Semigroup ( (<>) )
+#endif
+
+import Data.String             as X
+import Data.Word               as X
+import Prelude hiding (length, replicate, zipWith)
