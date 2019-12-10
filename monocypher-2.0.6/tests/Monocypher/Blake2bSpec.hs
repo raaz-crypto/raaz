d58e430d6ab4b4efa9cb30b179e668b365ee2178
Piyush P Kurur <ppk@iitpkd.ac.in>
2019-12-10 13:11:54 +0530

    added blake2b tests.

---
+++ monocypher-2.0.6/tests/Monocypher/Blake2bSpec.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module Monocypher.Blake2bSpec where
+
+
+import           Data.ByteString.Internal (unsafeCreate)
+import           Data.ByteString.Unsafe   (unsafeUseAsCStringLen)
+import qualified Foreign.Storable as Storable
+import           Foreign.Ptr
+import           Foreign.C.Types
+import           Foreign.C.String
+
+import           Tests.Core
+import qualified Raaz.Digest.Blake2b as Blake2b
+
+foreign import ccall unsafe
+    crypto_blake2b :: Ptr Word8  -- hash
+                   -> Ptr CChar  -- message
+                   -> CSize
+                   -> IO ()
+
+blake2bSize :: Int
+blake2bSize = Storable.sizeOf (undefined :: Blake2b)
+
+monocypher_blake2b_io :: Ptr Word8 -> CStringLen -> IO ()
+monocypher_blake2b_io hshPtr (ptr, l) = do crypto_blake2b hshPtr ptr (toEnum l)
+
+
+monocypher_blake2b :: ByteString -> Blake2b
+monocypher_blake2b bs = unsafeFromByteString $ unsafeCreate blake2bSize creator
+  where creator = \ ptr -> unsafeUseAsCStringLen bs (monocypher_blake2b_io ptr)
+
+spec :: Spec
+spec = do
+  prop "vs cyrpto_blake2b" $ \ x ->
+    monocypher_blake2b x `shouldBe` Blake2b.digest x
