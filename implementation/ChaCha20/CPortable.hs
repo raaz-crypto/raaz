4aa7ebfe8d4bcc24955181fc28385fa919b1d43a
Piyush P Kurur <ppk@iitpkd.ac.in>
2020-01-20 17:39:54 +0530

    cleanup mostly

--- implementation/ChaCha20/CPortable.hs
+++ implementation/ChaCha20/CPortable.hs
@@ -57,13 +57,13 @@ xchacha20Setup (XNounce tup) = do
   -- In the above step, the key gets replaced by the subkey obtained
   -- from the hchacha20 hash. We also set the ivcell appropriately
   withReaderT ivCell $ initialise iv
-  where [LE h0,LE h1,LE h2, LE h3, h4,h5] = V.toList $ unsafeToVector tup
-        iv  = Nounce $ unsafeFromList [0,h4,h5] :: Nounce ChaCha20
+  where [LE h0,LE h1,LE h2, LE h3, h4, h5] = V.toList $ unsafeToVector tup
+        iv  = Nounce $ unsafeFromList [0, h4, h5] :: Nounce ChaCha20
 
 
 -- | Copy the key from the memory cell chacha20Mem.
 copyKey :: Dest ChaCha20Mem -> Src (MemoryCell (Key ChaCha20)) -> IO ()
-copyKey cmem = copyMemory (keyCell <$> cmem)
+copyKey = copyMemory . fmap keyCell
 
 -------------- Helper function for running an iterator -----------
 runBlockProcess :: ( Ptr buf ->
