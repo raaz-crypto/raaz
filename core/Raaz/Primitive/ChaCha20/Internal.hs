315f4b9d80565d10e0693341180beec2061843ba
Piyush P Kurur <ppk@iitpkd.ac.in>
2020-01-21 15:37:48 +0530

    unused pragma.

--- core/Raaz/Primitive/ChaCha20/Internal.hs
+++ core/Raaz/Primitive/ChaCha20/Internal.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE DataKinds                        #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving       #-}
 {-# LANGUAGE FlexibleInstances                #-}
 {-# LANGUAGE MultiParamTypeClasses            #-}
 {-# LANGUAGE TypeFamilies                     #-}
