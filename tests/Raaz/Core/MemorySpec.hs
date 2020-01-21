d2a89083f3b331d4eb8f0cb6c1ae091912c5c5e6
Piyush P Kurur <ppk@iitpkd.ac.in>
2020-01-21 12:06:39 +0530

    memory tests to catch copy bug.

--- tests/Raaz/Core/MemorySpec.hs
+++ tests/Raaz/Core/MemorySpec.hs
@@ -1,10 +1,27 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Raaz.Core.MemorySpec where
-
+import Control.Monad.Reader
 import Tests.Core
 
 spec :: Spec
-spec = prop "store followed by read gives identical values"
-       $ \ (x :: Word) -> securely (storeAndRead x) `shouldReturn` x
+spec = do describe "store and read" $ do
+            prop "should return identical values when run securely"
+              $ \ (x :: Word) -> securely (storeAndRead x) `shouldReturn` x
+
+            prop "should return identical values when run insecurely"
+              $ \ (x :: Word) -> insecurely (storeAndRead x) `shouldReturn` x
+
+          describe "store, copy, and read" $ do
+            prop "should return identical values when run securely"
+              $ \ (x :: Word) -> securely (storeCopyRead x) `shouldReturn` x
+
+            prop "should return identical values when run insecurely"
+              $ \ (x :: Word) -> insecurely (storeCopyRead x) `shouldReturn` x
+
   where storeAndRead :: Word -> MT (MemoryCell Word) Word
         storeAndRead x = initialise x >> extract
+        storeCopyRead :: Word -> MT (MemoryCell Word, MemoryCell Word) Word
+        storeCopyRead x = do withReaderT fst $ initialise x
+                             mem <- ask
+                             liftIO $ copyMemory (destination $ snd mem) (source $ fst mem)
+                             withReaderT snd $ extract
