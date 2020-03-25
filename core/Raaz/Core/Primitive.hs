a690f2945d7dc5c50611b4658de0271f0f5530c5
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-03-26 00:01:49 +0530

    primitives block is now more nuanced.
    Instead of just capturing the primitive with a single block size,
    it is now a combination of a WordType and WordsPerBlock.

--- core/Raaz/Core/Primitive.hs
+++ core/Raaz/Core/Primitive.hs
@@ -7,35 +7,50 @@ use a more high level interface.
 
 -}
 
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies                #-}
 {-# LANGUAGE FlexibleContexts            #-}
 {-# LANGUAGE DataKinds                   #-}
 
 module Raaz.Core.Primitive
        ( -- * Cryptographic Primtives
-         Primitive(..), Key, Nounce
+         Primitive(..), Key, Nounce, Block, BlockPtr
+       , BLOCKS(..), blocksOf
        ) where
 
 import GHC.TypeLits
 
+import Foreign.Ptr      ( Ptr      )
+import Foreign.Storable ( Storable )
+import Raaz.Core.Prelude
+import Raaz.Core.Types.Endian
+import Raaz.Core.Types.Pointer
+import Raaz.Core.Types.Tuple
+
 ----------------------- A primitive ------------------------------------
 
 
--- | The type class that captures an abstract block cryptographic
--- primitive.
-class KnownNat (BlockSize p) => Primitive p where
+-- | Cryptographic primitives that process bulk data (like ciphers,
+-- cryptographic hashes) process data in blocks. For data that is not
+-- a multiple of the block size they may have some padding
+-- strategy. The type class that captures an abstract block
+-- cryptographic primitive.
+--
 
-  -- | Bulk cryptographic primitives like hashes, ciphers etc often
-  -- acts on blocks of data. The size of the block is captured by the
-  -- associated type `BlockSize`.
-  type BlockSize p :: Nat
+class (EndianStore (WordType p), KnownNat (WordsPerBlock p)) => Primitive p where
 
-  -- | The key associated with primitive. In the setting of the raaz
-  -- library keys are "inputs" that are required to start processing.
-  -- Often primitives like ciphers have a /secret key/ together with
-  -- an additional nounce/IV. This type denotes not just the secret
-  -- key par but the nounce too.
-  --
+  -- | The block which is the smallest unit of data that the primitive
+  -- processes, is typically considered as an array of a particular
+  -- word which is captured by the following associated type.
+  type WordType p :: *
+
+  -- | The size of the array that forms the block. In particular, the
+  -- block can be seen as an array of size `BlockArraySize p` of type
+  -- `WORD p`.
+  type WordsPerBlock p :: Nat
+
+
+-- type BlockPtr p = Ptr (Tuple (BlockArraySize p) (WORD p)
 
 -- | The type family that captures the key of a keyed primitive.
 data family Key p :: *
@@ -45,3 +60,8 @@ data family Key p :: *
 -- sharing the key. The type family that captures the nounce for a
 -- primitive (if it requires one).
 data family Nounce p :: *
+
+type Block p   = Tuple (WordsPerBlock p) (WordType p)
+-- | Pointer to a block of the primitive.
+type BlockPtr p = Ptr (Block p)
+
