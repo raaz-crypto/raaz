c15af08cc0e8191ca0236c05f30d4fbaabe1f06d
Piyush P Kurur <ppk@cse.iitk.ac.in>
2020-07-29 23:40:46 +0530

    the buffer subpackage updated.

--- indef/buffer/Buffer.hs
+++ indef/buffer/Buffer.hs
@@ -3,31 +3,54 @@
 {-# LANGUAGE DataKinds        #-}
 {-# LANGUAGE MonoLocalBinds   #-}
 module Buffer
-       ( allocBufferFor
-       , BufferPtr
-       , Buffer, getBufferPointer, bufferSize
-       , clearBuffer
+       ( BufferPtr
+       , Buffer
+       , withBufferPtr
+       , unsafeWithBufferPtr
+       , memsetBuffer
+       -- ** Some unsafe functions
+       , unsafeGetBufferPointer
+       , bufferSize
        ) where
 
-import Control.Monad.Reader
 import GHC.TypeLits
 
 import Raaz.Core
+import Raaz.Core.Memory (Access(..))
 import Implementation
 
--- | A memory buffer than can handle up to @n@ blocks of data. This
--- happens when you need to do some transformation on internal data
--- using a primitive. An example is using a stream cipher for
--- pseudo-random generation. Note that and additional blocks that
--- might be required at the end for padding is taken care of and one
--- does not need to worry about it here. I.e. @Buffer n@ has enough
--- space for the @n@ blocks and if required any additional blocks that
--- meant for padding.
-newtype Buffer (n :: Nat) = Buffer { unBuffer :: Pointer }
+-- | A buffer @buf :: Buffer n@ is a memory element that has enough
+-- space for the @n@ blocks of the primitive and, if required by the
+-- implementation, any additional blocks that might be used for
+-- padding of the last chunk of message.
+newtype Buffer (n :: Nat) = Buffer { unBuffer :: Ptr Word8 }
 
--- | Get the underlying pointer for the buffer.
-getBufferPointer :: Buffer n -> BufferPtr
-getBufferPointer = castAlignedPtr . nextAlignedPtr . unBuffer
+
+-- | This takes a buffer pointer action and runs it with the underlying pointer associated with
+-- the buffer. The action is supposed to use
+unsafeWithBufferPtr :: KnownNat n
+                    => (BufferPtr -> a)
+                    -> Buffer n
+                    -> a
+unsafeWithBufferPtr action = action . unsafeGetBufferPointer
+
+-- | Run the action on the buffer pointer.
+withBufferPtr :: KnownNat n
+              => (BufferPtr -> BlockCount Prim -> a)
+              -> Buffer n
+              -> a
+withBufferPtr action buf = unsafeWithBufferPtr act buf
+  where act = flip action $ bufferSize $ pure buf
+
+-- | Memset the given buffer.
+memsetBuffer :: KnownNat n => Word8 -> Buffer n -> IO ()
+memsetBuffer = withBufferPtr . flip memset
+
+
+-- WARNING: Not to be exposed else can be confusing with
+-- `bufferSize`. Internal function used by allocation.
+actualBufferSize :: KnownNat n => Proxy (Buffer n) -> BlockCount Prim
+actualBufferSize bproxy = bufferSize bproxy <> additionalBlocks
 
 {-# INLINE bufferSize #-}
 -- | The size of data (measured in blocks) that can be safely
@@ -37,10 +60,10 @@ bufferSize = flip blocksOf Proxy . fromIntegral . natVal . nProxy
   where nProxy :: Proxy (Buffer n) -> Proxy n
         nProxy  _ = Proxy
 
--- | Internal function used by allocation.  WARNING: Not to be exposed
--- else can be confusing with `bufferSize`.
-actualBufferSize :: KnownNat n => Proxy (Buffer n) -> BlockCount Prim
-actualBufferSize bproxy = bufferSize bproxy <> additionalBlocks
+-- | Get the underlying pointer for the buffer.
+unsafeGetBufferPointer :: Buffer n -> BufferPtr
+unsafeGetBufferPointer = castPointer . nextAlignedPtr . unBuffer
+
 
 instance KnownNat n => Memory (Buffer n) where
   memoryAlloc = allocThisBuffer
@@ -53,16 +76,15 @@ instance KnownNat n => Memory (Buffer n) where
   unsafeToPointer = unBuffer
 
 
-allocBufferFor :: MonadIOCont m
-               => BlockCount Prim
-               -> (BufferPtr  -> m a) -> m a
-
-allocBufferFor blks = allocaAligned totalSize
-  where totalSize = blks `mappend` additionalBlocks
-
--- | Set the contents of the buffer to zeros.
-clearBuffer :: KnownNat n => MT (Buffer n) ()
-clearBuffer = do buf <- ask
-                 let sz   = bufferSize $ pure buf
-                     bptr = getBufferPointer buf
-                   in memset (forgetAlignment bptr) 0 sz
+instance KnownNat n => Accessible (Buffer n) where
+  accessList buf = [ Access { accessPtr    = castPointer bufPtr
+                            , accessSize   = inBytes $ bufferSize $ pure buf
+                            , accessAdjust = adjust (Proxy :: Proxy Prim) bufPtr nelems
+                            }
+                   ]
+       where getProxy :: Buffer n -> Proxy n
+             getProxy _ = Proxy
+             nelems     = fromEnum $ natVal $ getProxy buf
+             bufPtr     = unsafeRawPtr $ unsafeGetBufferPointer buf
+             adjust    :: Primitive prim => Proxy prim -> BlockPtr prim -> Int -> IO ()
+             adjust _   = adjustEndian
