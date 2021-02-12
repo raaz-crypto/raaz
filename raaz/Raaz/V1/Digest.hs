6197c260c8f8c2943ca957ad31eeee9054c1d024
Piyush P Kurur <ppk@cse.iitk.ac.in>
2021-02-12 18:55:23 +0530

    simplify v1-digest.

--- raaz/Raaz/V1/Digest.hs
+++ raaz/Raaz/V1/Digest.hs
@@ -5,32 +5,7 @@
 --
 -- For documentation refer to the module "Raaz.Digest".
 
-module Raaz.V1.Digest ( Digest
-                      , digest
-                      , digestFile
-                      , digestSource
-                      ) where
-
-import qualified Raaz.Digest.Blake2b as B2b
-import           Raaz.Core
-import           Raaz.Primitive.Blake2.Internal(Blake2b)
-
--- | The message digest.
-type Digest = Blake2b
-
--- | Compute the digest of a pure byte source like, `B.ByteString`.
-digest :: PureByteSource src
-       => src  -- ^ Message
-       -> Digest
-digest = B2b.digest
-
--- | Compute the digest of file.
-digestFile :: FilePath  -- ^ File to be digested
-           -> IO Digest
-digestFile = B2b.digestFile
-
--- | Compute the digest of an arbitrary byte source.
-digestSource :: ByteSource src
-             => src
-             -> IO Digest
-digestSource = B2b.digestSource
+module Raaz.V1.Digest (
+   module Raaz.Digest.Blake2b
+  ) where
+import Raaz.Digest.Blake2b
